b876473d2ff3bef8492186cff2ee76b1b5998510
John Doe <murraylyne2@Gmail.com>
2016-11-28 21:46:36 +0000

    twitter feed

--- newcss/index.php
+++ newcss/index.php
@@ -89,7 +89,7 @@ include 'master.php';
 
 <div id="top" style="float:left">
 
-    <div id="map" style="height:700px;width:500px; float:left;">
+    <div id="map" style="height:700px;width:800px; float:left;">
 
     </div>
 
