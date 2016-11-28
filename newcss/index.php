62e8a86e44c37262d05a48775963703b251ee374
John Doe <murraylyne2@Gmail.com>
2016-11-28 20:07:39 +0000

    added basic map

--- newcss/index.php
+++ newcss/index.php
@@ -6,6 +6,78 @@ include 'master.php';
 <html lang="en">
 <link href="master.css" rel="stylesheet" />
 
+<title>GoPortlethen</title>
+
+
+<script type="text/javascript">
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 57.061681, lng: -2.129468},
+            zoom: 12,
+            MapTypeId: "satellite"
+        });
+    }
+</script>
+<script type="text/javascript">
+    //Get the markers from the XML document.
+
+    function downloadUrl(url, callback) {
+        var request = window.ActiveXObject ?
+            new ActiveXObject('Microsoft.XMLHTTP') :
+            new XMLHttpRequest;
+
+        request.onreadystatechange = function () {
+            if (request.readyState == 4) {
+                callback(request, request.status);
+            }
+        };
+
+        request.open('GET', url, true);
+        request.send(null);
+    }
+
+    //download the locations.
+
+    downloadUrl("../scripts/markers.php", function (data) {
+        var xml = data.responseXML;
+
+        var markers = xml.documentElement.getElementsByTagName("markers");
+        for (var i = 0; i < markers.length; i++) {
+
+            var point = new google.maps.LatLng(
+
+                parseFloat(markers[i].getAttribute("lat")),
+                parseFloat(markers[i].getAttribute("lng")));
+
+            addMarker(i, point);
+
+        }
+    });
+
+    var markersArray = [];  //array for markers
+
+    //this fucntion will add the markers.
+    function addMarker(id,point) {
+        id = new google.maps.Marker({
+            map: map,
+            draggable: false,
+            animation: google.maps.Animation.DROP,
+            position: point
+
+        });
+        id.addListener('click', function() {
+            map.setZoom(3);
+            map.setCenter(id.getPosition());
+
+        });
+        markersArray.push(id);
+    }
+
+</script>
+<script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCC8HwZx1Aknt-BHgT2vYtcgeBBvokVzWU&callback=initMap">
+</script>
+
 <head>
     <meta charset="UTF-8">
     <title></title>
@@ -23,5 +95,7 @@ Discover:
         </ul>
     </div>
 </div>
+
+<div id="map" style="height:700px;width:1250px;"
 </body>
 </html>
