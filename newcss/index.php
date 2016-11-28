ef47bc6ef1f0aa62f5b9ee7e35efd6e95493f751
Naelegs <jamiepeterlaw@gmail.com>
2016-11-28 21:35:24 +0000

    twitterfeed-test

--- newcss/index.php
+++ newcss/index.php
@@ -90,14 +90,14 @@ include 'master.php';
 <div id="top">
 
     <div id="map" style="height:700px;width:500px;">
-        <a class="twitter-timeline" data-width="300" data-height="800" d href="https://twitter.com/PortlethenGC">Tweets by PortlethenGC</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-    </div>
-
-
-    <!--<div id="twitter">
 
     </div>
-    -->
+
+
+   <div id="twitter">
+    <a class="twitter-timeline" data-width="300" data-height="800" d href="https://twitter.com/PortlethenGC">Tweets by PortlethenGC</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </div>
+
 </div>
 </body>
 </html>
