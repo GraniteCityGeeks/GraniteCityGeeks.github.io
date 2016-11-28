4c98b15ea567bd11d1fe4d8d7623257fab72f56e
Medscootsman <murraylyne2@Gmail.com>
2016-11-28 14:49:40 +0000

    added markers.php
    **TEST DATA ONLY**

--- scripts/dbconnect.php
+++ scripts/dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = new mysqli("eu-cdbr-azure-north-e.cloudapp.net","baa0a9a8051f1c","a6f9898c","dbjamielaw");
+$db = new mysqli("eu-cdbr-azure-west-a.cloudapp.net","b169b5e27239e9","a6f9898c","dbjamielaw");
 
 if ($db->connect_errno) {
     die ('Connection Failed :'.$db->connect_error );
