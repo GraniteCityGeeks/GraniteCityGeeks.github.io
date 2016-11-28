6e28e529ca284f4e7cfb4cf2a7606c0f74799fdd
John Doe <murraylyne2@Gmail.com>
2016-11-28 21:45:06 +0000

    twitter feed

--- newcss/index.php
+++ newcss/index.php
@@ -89,12 +89,12 @@ include 'master.php';
 
 <div id="top" style="float:left">
 
-    <div id="map" style="height:700px;width:500px;">
+    <div id="map" style="height:700px;width:500px; float:left;">
 
     </div>
 
 
-   <div id="twitter">
+   <div id="twitter" style="float:left;">
     <a class="twitter-timeline" data-width="300" data-height="800" d href="https://twitter.com/PortlethenGC">Tweets by PortlethenGC</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
     </div>
 
