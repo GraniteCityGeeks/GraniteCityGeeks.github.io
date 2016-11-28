0ec29d2ba252edebd02693227f4ad7a25a9a88d5
Medscootsman <murraylyne2@Gmail.com>
2016-11-28 12:55:43 +0000

    added error check for dbconnect

--- scripts/dbconnect.php
+++ scripts/dbconnect.php
@@ -1,6 +1,10 @@
 <?php
 
-$db = new mysqli("eu-cdbr-azure-north-e.cloudapp.net","baa0a9a8051f1c","ea0c77dc","marvel")
+$db = new mysqli("eu-cdbr-azure-north-e.cloudapp.net","baa0a9a8051f1c","ea0c77dc","marvel");
+
+if ($db->connect_errno) {
+    die ('Connection Failed :'.$db->connect_error );
+}
 
 
 ?>
