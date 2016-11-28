7c186f8abb72926a8154aaa27d6d32d214fe22ca
Medscootsman <murraylyne2@Gmail.com>
2016-11-28 14:41:21 +0000

    added markers.php
    **TEST DATA ONLY**

---
+++ scripts/markers.php
@@ -0,0 +1,32 @@
+<?php
+include("dbconnect.php");
+
+$dom = new DOMDocument("1.0");
+
+$node = $dom->createElement("markers");
+$parnode = $dom->appendChild($node);
+
+$query = "SELECT * FROM markers WHERE 1";
+$result = $db->query($query);
+if (!$result) {
+    die('Nothing in result: ');
+}
+
+header("Content-type: text/xml");
+
+// Iterate through the rows, adding XML nodes for each
+
+while ($row = $result->fetch_array()){
+    // ADD TO XML DOCUMENT NODE
+    $node = $dom->createElement("markers");
+    $newnode = $parnode->appendChild($node);
+    $newnode->setAttribute("name",$row['name']);
+    $newnode->setAttribute("address", $row['address']);
+    $newnode->setAttribute("lat", $row['lat']);
+    $newnode->setAttribute("lng", $row['lng']);
+    $newnode->setAttribute("type", $row['type']);
+}
+$result->close();
+$db->close();
+
+echo $dom->saveXML();
