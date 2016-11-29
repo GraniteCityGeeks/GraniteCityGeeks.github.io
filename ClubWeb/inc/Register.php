ccecc6218010765c448c31cbe2313bb85c610575
JordanYoungman <jwyoungman96@homail.co.uk>
2016-11-29 16:31:45 +0000

    hi

--- ClubWeb/inc/Register.php
+++ ClubWeb/inc/Register.php
@@ -1,11 +1,26 @@
 <?php
-echo "
-<main>
-    <form action='insertuser.php' method='post'>
-        <input type='text' name='username' placeholder='Username'><br>
-        <input type='text' name='password' placeholder='Password'><br>
-        <input type='submit' text='Submit'>
-    </form>
-</main>
-    ";
-?>
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    include("scripts/header.php");
+    ?>
+    <main>
+        <form action="register" method="post">
+            <input type="text" name="username" placeholder="username"></br>
+            <input type="password" name="password" placeholder="password"></br>
+            <p><input type="submit" value="Submit"></p>
+        </form>
+    </main>
+    <?
+    include("scripts/footer.php");
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    include("scripts/dbconnect.php");
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $sql = "INSERT INTO port_users '" . $username . "' and
+password='" . $password . "'";
+        $result = $db->query($sql);
+        while ($row = $result->fetch_array()) {
+            return true;
+        }
+        return false;
+}
