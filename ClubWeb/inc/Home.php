fb54fa8bee4dc1063cb351fdd7ee51132ede4e1e
JordanYoungman <jwyoungman96@homail.co.uk>
2016-11-29 13:49:28 +0000

    updated 28/11/16

--- ClubWeb/inc/Home.php
+++ ClubWeb/inc/Home.php
@@ -2,6 +2,8 @@
 include ("scripts/header.php");
 session_start();
 $username_cookie = $_COOKIE['username_cookie'];
+
+displayUsername($username_cookie);
 echo "
 <main>
 <p>Welcome {$username_cookie}! In this blog you will see all of my insights and wonderful things</p>
