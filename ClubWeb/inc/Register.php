5ce096920cd75f91f6f126ed98d6e575b142f89c
1611267 <phaseiimusic@hotmail.com>
2016-11-29 14:44:05 +0000

    Registration page

--- ClubWeb/inc/Register.php
+++ ClubWeb/inc/Register.php
@@ -6,23 +6,10 @@
  * Time: 12:44
  */
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    include("scripts/header.php");
-    ?>
-    <main>
-        <form action="register" method="post">
-            <input type="text" name="username" placeholder="username"></br>
-            <input type="password" name="password" placeholder="password"></br>
-            <p><input type="submit" value="Submit"></p>
-        </form>
-    </main>
-    <?
-    include("scripts/footer.php");
-} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    include("scripts/dbconnect.php");
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    $sql = "INSERT INTO port_users (username, password) VALUES ('" . $username . "','" . $password . "')";
-    $result = $db->query($sql);
-}
+include('dbconnect.php');
+
+$username = 'username';
+$password = 'password';
+
+$sql = "INSERT INTO port_users (username, password) VALUES ('".."','""')";
 ?>
