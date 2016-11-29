ecc7551c1f34f62e796110b5c20edb1793d0af6c
JordanYoungman <jwyoungman96@homail.co.uk>
2016-11-29 16:09:42 +0000

    hi

---
+++ ClubWeb/inc/Register.php
@@ -0,0 +1,11 @@
+<?php
+echo "
+<main>
+    <form action="insertsuperhero.php" method="post">
+        <input type="text" name="firstname" placeholder="First name"><br>
+        <input type="text" name="lastname" placeholder="Last name"><br>
+        <input type="text" value="superpower" placeholder="Main Super Power"><br>
+        <input type="submit" text="Submit">
+    </form>
+</main>
+    ";
