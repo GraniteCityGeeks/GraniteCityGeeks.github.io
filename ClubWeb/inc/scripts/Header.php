177856d9db3cede3b83c635a7fc41a750e6e6d3d
JordanYoungman <jwyoungman96@homail.co.uk>
2016-11-29 14:10:20 +0000

    updated 28/11/16

--- ClubWeb/inc/scripts/Header.php
+++ ClubWeb/inc/scripts/Header.php
@@ -26,6 +26,7 @@ session_start();
                 echo "<li><a href='logout'>Logout</a></li>";
             } else {
                 echo "<li><a href='login'>Login</a></li>";
+                echo "<li><a href='register'>Register</a></li>";
             }
             ?>
 
