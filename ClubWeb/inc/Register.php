f050d4e2cc054b9de3523fe82b6ef0e2509c5109
1611267 <phaseiimusic@hotmail.com>
2016-11-29 15:15:03 +0000

    Registration page

--- ClubWeb/inc/Register.php
+++ ClubWeb/inc/Register.php
@@ -11,5 +11,11 @@ include('dbconnect.php');
 $username = 'username';
 $password = 'password';
 
-$sql = "INSERT INTO port_users (username, password) VALUES ('".."','""')";
+$sql = "INSERT INTO port_users (username, password) VALUES ('". $username ."','" . $password . "')";
+
+if(mysqli_query($db, $sql)) {
+} else {
+    echo "error" . $sql . "<br>" . mysqli_error($db);
+}
+
 ?>
