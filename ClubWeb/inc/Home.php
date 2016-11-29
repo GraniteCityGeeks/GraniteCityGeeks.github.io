5f99acf971709763d0bad931405177ba0d0d1445
JordanYoungman <jwyoungman96@homail.co.uk>
2016-11-29 13:35:09 +0000

    updated 28/11/16

--- ClubWeb/inc/Home.php
+++ ClubWeb/inc/Home.php
@@ -3,7 +3,7 @@ include ("scripts/header.php");
 $username = $_POST["username"];
 echo "
 <main>
-<p>Welcome {$username}! In this blog you will see all of my insights and wonderful things</p>
+<p>Welcome", $username, "! In this blog you will see all of my insights and wonderful things</p>
 </main>
 ";
 include ("scripts/footer.php");
