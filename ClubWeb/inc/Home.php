2005565ad69ab0594237bde57ea274da816db49a
Jordan <jwyoungman96@hotmail.co.uk>
2016-11-30 02:15:49 +0000

    test

--- ClubWeb/inc/Home.php
+++ ClubWeb/inc/Home.php
@@ -1,11 +1,12 @@
 <?php
 include ("scripts/header.php");
 session_start();
-$username_cookie = $_COOKIE['username_cookie'];
+//$username_cookie = $_COOKIE['username_cookie'];
+$_SESSION['$username'] = $myusername;
 
 echo "
 <main>
-<p>Welcome {$username_cookie}! In this blog you will see all of my insights and wonderful things</p>
+<p>Welcome {$myusername}! In this blog you will see all of my insights and wonderful things</p>
 </main>
 ";
 include ("scripts/footer.php");
