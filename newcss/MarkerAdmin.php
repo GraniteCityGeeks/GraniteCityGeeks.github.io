f9671709bdd828213a2fe33ae334a5cf4aac18fe
John Doe <murraylyne2@Gmail.com>
2016-12-01 15:05:14 +0000

    twitter feed

--- newcss/MarkerAdmin.php
+++ newcss/MarkerAdmin.php
@@ -13,23 +13,74 @@ include 'master.php';
     </head>
 
 <body>
-<div id="editmap" style="float:left;">
-    <form action="../scripts/markercreate.php" method="get">
-        Marker Name:<br>
-        <input type="text" name="name" placeholder="e.g Local Wood"><br><br>
-        Address:<br>
-        <input type="text" name="address" placeholder="123 Example St."><br><br>
-        Latitude:<br>
-        <input type="text" name="lat" placeholder=""><br><br>
-        Longitude:<br>
-        <input type="text" name="lng" placeholder=""><br><br>
-        Type:<br>
-        <input type="text" name="type" placeholder="e.g Playpark"><br><br>
-        Marker Description :<br>
-        <input type="text" name="markerdesc" placeholder="describe marker"><br><br>
-        <input type="submit" value="Submit">
-    </form>
+<div id="main">
+    <div id="editmap" style="float:left;">
+        <form action="../scripts/markercreate.php" method="get">
+            Marker Name:<br>
+            <input type="text" name="name" placeholder="e.g Local Wood"><br><br>
+            Address:<br>
+            <input type="text" name="address" placeholder="123 Example St."><br><br>
+            Latitude:<br>
+            <input type="text" name="lat" placeholder=""><br><br>
+            Longitude:<br>
+            <input type="text" name="lng" placeholder=""><br><br>
+            Type:<br>
+            <input type="text" name="type" placeholder="e.g Playpark"><br><br>
+            Marker Description :<br>
+            <input type="text" name="markerdesc" placeholder="describe marker"><br><br>
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+    <div id="marker-management">
+        <h1>Markers</h1>
+        <table border="1">
+            <tr>
+                <th>Marker</th>
+                <th>Lat</th>
+                <th>Lng</th>
+                <th>description</th>
+            </tr>
+
+        <?php
+        //connect to the database.
+        include(dbconnect.php);
+        $query = "SELECT * FROM port_markers";
+        $result = $db->query($query);
+
+        if ($result->num_rows > 0) {
+
+            while($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>". $row["name"]."</td>";
+                echo "<td>". $row["lat"]. "</td>";
+                echo "<td>". $row["lng"]. "</td>";
+                echo "<td>". $row["description"]. "</td>";
+                echo "</tr>";
+
+
+
+            }
+        }
+
+        $db->close();
+        ?>
+
+            </table>
+
+
+
+
+
+
+
+
+
+
+
+        ?>
+    </div>
 </div>
+
 
 </body>
 </html>
