6658a83a13a4702897d850d09b27b66d9a0ee38d
John Doe <murraylyne2@Gmail.com>
2016-12-01 17:04:43 +0000

    twitter feed

--- scripts/markerdelete.php
+++ scripts/markerdelete.php
@@ -2,3 +2,16 @@
 include(dbconnect.php);
 
 $id = $_GET['delete'];
+
+$query = "DELETE FROM port_markers WHERE  'id' =". $id;
+
+$result = $db->query($query);
+
+if (!$result) {
+    die("THIS ISN'T WORKING");
+}
+
+else {
+    header("Location: ../newcss/MarkerAdmin.php");
+    die();
+}
