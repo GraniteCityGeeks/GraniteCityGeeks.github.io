6fc0b17ba01c784d8306a89faab982ba18ef8236
1611267 <phaseiimusic@hotmail.com>
2016-12-02 15:11:39 +0000

    Merge branch 'master' of C:\Users\1611267\PhpstormProjects\GraniteCityGeeks.github.io with conflicts.

--- ClubWeb/inc/Login.php
+++ ClubWeb/inc/Login.php
@@ -15,22 +15,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     include("scripts/dbconnect.php");
     $username = $_POST["username"];
     $password = $_POST["password"];
-
+    $accesslevel = -1;
     function checklogin($username, $password, $db)
     {
         $sql = "SELECT * FROM port_users WHERE username='" . $username . "' and
 password='" . $password . "'";
         $result = $db->query($sql);
         while ($row = $result->fetch_array()) {
-            session_start();
-            $_SESSION['accessLevelID']  = $row['accessLevelID'];
+            global $accesslevel;
+            $accesslevel = $row['accessLevelID'];
             return true;
         }
         return false;
     }
 
     if (checklogin($username, $password, $db)) {
+        session_start();
         $_SESSION['username'] = $username;
+        $_SESSION['accessLevelID'] = $accesslevel;
         header("location:./");
     } else {
         header("location:login");
