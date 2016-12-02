dc139a0c1ae38b6e3a4278b3bd6ba7a77dc5dbf7
JordanYoungman <jwyoungman96@homail.co.uk>
2016-12-02 14:49:34 +0000

    hi

--- ClubWeb/inc/scripts/Header.php
+++ ClubWeb/inc/scripts/Header.php
@@ -25,7 +25,7 @@ session_start();
                 echo "<li><a href='logout'>Logout</a></li>";
             } else {
                 echo "<li><a href='login'>Login</a></li>";
-                echo "<li><a href='register'>Register</a></li>";
+                echo "<li><a href='register'>Regi</a></li>";
             }
             ?>
 
