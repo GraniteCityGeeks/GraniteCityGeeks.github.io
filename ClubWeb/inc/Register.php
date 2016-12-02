2823a78f38ec88bb04ef4608dfc7a0af69330508
1611267 <phaseiimusic@hotmail.com>
2016-12-02 16:10:52 +0000

    Registration page

--- ClubWeb/inc/Register.php
+++ ClubWeb/inc/Register.php
@@ -1,6 +1,6 @@
 <?php
     include("scripts/Header.php");
-    ?>
+?>
     <main>
         <form action="register" method="post">
             <input type="text" name="username" placeholder="username"></br>
@@ -8,36 +8,37 @@
             <p><input type="submit" value="Submit"></p>
         </form>
     </main>
-    <?php
+
+<?php
     include("scripts/Footer.php");
     include("scripts/dbconnect.php");
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-//    function checkUsers($username, $db) {
-//        $sql = "SELECT username FROM port_users";
-//        $result = $db->query($sql);
-//        while ($row = $result->fetch_array()) {
-//            if($row['username'] == $username) {
-//                return false;
-//            }
-//        }
-//        return true;
-//    }
-//
-//    if(checkUsers($username, $db)) {
-//        $sql = "INSERT INTO port_users (username, password) VALUES ('$username', '$password')";
-//        if (mysqli_query($db, $sql)) {
-//            echo "New record created succesfully";
-//        } else {
-//            echo "Error: " . $sql . "<br>" . mysqli_error($db);
-//        }
-//        mysqli_close($db);
-//    } else {
-//        header("location:register");
-//        echo "Sorry! That username is already in use.";
-//    }
+    function checkUsers($username, $db) {
+        $sql = "SELECT username FROM port_users";
+        $result = $db->query($sql);
+        while ($row = $result->fetch_array()) {
+            if($row['username'] == $username) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    if(checkUsers($username, $db)) {
+        $sql = "INSERT INTO port_users (username, password) VALUES ('$username', '$password')";
+        if (mysqli_query($db, $sql)) {
+            echo "New record created succesfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($db);
+        }
+        mysqli_close($db);
+    } else {
+        //header("location:register");
+        echo "Sorry! That username is already in use.";
+    }
 
 
-// this is impossible
+
 ?>
