d742e3782fc164d70034add4eb93686d2d74d872
Jordan <jwyoungman96@hotmail.co.uk>
2016-12-02 22:58:19 +0000

    test

--- ClubWeb/inc/View.php
+++ ClubWeb/inc/View.php
@@ -14,49 +14,19 @@
 
 <?php
 
-/*
-
-VIEW.PHP
-
-Displays all data from 'players' table
-
-*/
-
-
-
-// connect to the database
-
 include("scripts/dbconnect.php");
 
-
-
-// get results from database
-
-$result = "SELECT * FROM port_users";
-
-
-// display data in table
+$sql = "SELECT * FROM port_users";
 
 echo "<p><b>View All</b></p>";
-
-
 
 echo "<table border='1' cellpadding='10'>";
 
 echo "<tr> <th>ID</th> <th>username</th> <th>password</th> <th></th> <th></th></tr>";
 
-
-
-// loop through results of database query, displaying them in the table
 $result = $db->query($sql);
 while ($row = $result->fetch_array()) {
-
-
-
-// echo out the contents of each row into a table
-
     echo "<tr>";
-
     echo '<td>' . $row['userID'] . '</td>';
 
     echo '<td>' . $row['username'] . '</td>';
