c7edeaad5a5a81ad06316989290a3eac354befa6
1611267 <phaseiimusic@hotmail.com>
2016-12-02 16:13:40 +0000

    Registration page

--- ClubWeb/inc/Register.php
+++ ClubWeb/inc/Register.php
@@ -10,14 +10,17 @@
     </main>
 
 <?php
+
     include("scripts/Footer.php");
     include("scripts/dbconnect.php");
+
     $username = $_POST["username"];
     $password = $_POST["password"];
 
     function checkUsers($username, $db) {
         $sql = "SELECT username FROM port_users";
         $result = $db->query($sql);
+
         while ($row = $result->fetch_array()) {
             if($row['username'] == $username) {
                 return false;
@@ -28,14 +31,15 @@
 
     if(checkUsers($username, $db)) {
         $sql = "INSERT INTO port_users (username, password) VALUES ('$username', '$password')";
+
         if (mysqli_query($db, $sql)) {
             echo "New record created succesfully";
         } else {
             echo "Error: " . $sql . "<br>" . mysqli_error($db);
         }
+
         mysqli_close($db);
     } else {
-        //header("location:register");
         echo "Sorry! That username is already in use.";
     }
 
