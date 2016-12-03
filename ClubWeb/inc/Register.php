dab78957b9fb80cebb1d05baecc88669e1c1d172
Jordan <jwyoungman96@hotmail.co.uk>
2016-12-03 00:30:50 +0000

    test

--- ClubWeb/inc/Register.php
+++ ClubWeb/inc/Register.php
@@ -14,16 +14,16 @@
     include("scripts/Footer.php");
     include("scripts/dbconnect.php");
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+if(isset($_POST['username']) && isset($_POST['password'])){
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
 
-    $query = mysqli_query("SELECT * FROM port_users WHERE username ='$username'");
+    $query = mysqli_query("SELECT * FROM port_users WHERE username='$username'");
     if(mysqli_num_rows($query) > 0 ) { //check if there is already an entry for that username
         echo "Username already exists!";
     }else{
-        mysqli_query("INSERT INTO users (username, password) VALUES ('$username', '$password')");
-        header("location:login");
+        mysqli_query("INSERT INTO port_users (username, password) VALUES ('$username', '$password')");
+        header("location:index.php");
     }
 }
 mysqli_close();
