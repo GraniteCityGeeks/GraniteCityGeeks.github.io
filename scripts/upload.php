c925b8306491990a5d1388eaa12c6b22e860166a
Naelegs <jamiepeterlaw@gmail.com>
2016-12-05 18:27:41 +0000

    file upload

--- scripts/upload.php
+++ scripts/upload.php
@@ -13,10 +13,27 @@ if (isset($_FILES['file'])){
     $file_name = $file['name'];
     $file_tmp = $file['tmp_name'];
     $file_size = $file['size'];
+    $file_error = $file['error'];
 
     $file_ext = explode('',$file_name);
+    $file_ext = strtolower(end($file_ext));
+
+    $allowed = array('jpg','bmp','gif','png');
 
     print_r($file_ext);
+
+    if(in_array($file_ext,$allowed)){
+        if ($file_error === 0) {
+
+            $file_name_new = uniqid('',true).'.'.$file_ext;
+            $file_destination = 'uploads/'.$file_name_new;
+
+            if(move_uploaded_file($file_tmp,$file_destination)){
+                echo $file_destination . 'Was Uploaded Successfully!';
+            }
+
+        }
+    }
 
     
 }
