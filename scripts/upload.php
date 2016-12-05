969ac12462ba5c5e391550dc527f18cd6c557c63
Naelegs <jamiepeterlaw@gmail.com>
2016-12-05 20:46:31 +0000

    file upload

--- scripts/upload.php
+++ scripts/upload.php
@@ -5,9 +5,10 @@ $name = $_FILES['file']['name'];
 $size = $_FILES['file']['size'];
 $type = $_FILES['file']['type'];
 
-$tmp_name = $_FILES['file']['tmp_name'];
+echo $tmp_name = $_FILES['file']['tmp_name'];
 $error = $_FILES['file']['error'];
 
+die();
 if (isset($name)){
     if (!empty($name)){
         echo 'ok';
