d3d5ee1afb64d8bd6f044c36f8ecfe34049fc871
1611267 <phaseiimusic@hotmail.com>
2016-12-05 13:48:16 +0000

    Registration page

--- ClubWeb/inc/Register.php
+++ ClubWeb/inc/Register.php
@@ -25,27 +25,27 @@ function checkUsers($username, $db) {
     $result = mysqli_query($db, $sql);
 
     while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-//        if($row['username'] == $username) {
-//            return false;
-//        }
-        echo "<p>" . $row['username'] . "</p>";
+        if($row['username'] == $username) {
+            return false;
+        }
+        //echo "<p>" . $row['username'] . "</p>";
     }
-//    return true;
+    return true;
 }
 
-//if(checkUsers($username, $db)) {
-//    $sql = "INSERT INTO port_users (username, password) VALUES ('$username', '$password')";
-//
-//    if (mysqli_query($db, $sql)) {
-//        echo "New record created succesfully";
-//    } else {
-//        echo "Error: " . $sql . "<br>" . mysqli_error($db);
-//    }
-//
-//    mysqli_close($db);
-//} else {
-//    echo "Sorry! That username is already in use.";
-//}
+if(checkUsers($username, $db)) {
+    $sql = "INSERT INTO port_users (username, password) VALUES ('$username', '$password')";
+
+    if (mysqli_query($db, $sql)) {
+        echo "New record created succesfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($db);
+    }
+
+    mysqli_close($db);
+} else {
+    echo "Sorry! That username is already in use.";
+}
 
 
 
