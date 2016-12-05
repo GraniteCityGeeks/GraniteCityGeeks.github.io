2a6d6d7d686576f384ab57cd1f24487d45d34351
1611267 <phaseiimusic@hotmail.com>
2016-12-05 14:03:02 +0000

    Registration page

--- ClubWeb/inc/Register.php
+++ ClubWeb/inc/Register.php
@@ -3,7 +3,7 @@ include("scripts/Header.php");
 ?>
 
     <main>
-        <form action="register" method="post">
+        <form action="AddUser.php" method="post">
             <input type="text" name="username" placeholder="username"></br>
             <input type="password" name="password" placeholder="password"></br>
             <p><input type="submit" value="Submit"></p>
@@ -11,42 +11,5 @@ include("scripts/Header.php");
     </main>
 
 <?php
-
-include("scripts/Footer.php");
-include("scripts/dbconnect.php");
-
-$username = $_POST["username"];
-$password = $_POST["password"];
-
-checkUsers($username, $db);
-
-function checkUsers($username, $db) {
-    $sql = "SELECT username FROM port_users";
-    $result = mysqli_query($db, $sql);
-
-    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-        if($row['username'] == $username) {
-            return false;
-        }
-        //echo "<p>" . $row['username'] . "</p>";
-    }
-    return true;
-}
-
-if(checkUsers($username, $db)) {
-    $sql = "INSERT INTO port_users (username, password) VALUES ('$username', '$password')";
-
-    if (mysqli_query($db, $sql)) {
-        echo "New record created succesfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($db);
-    }
-
-    mysqli_close($db);
-} else {
-    echo "Sorry! That username is already in use.";
-}
-
-
-
+    include("scripts/Footer.php");
 ?>
