395e5c03fdcc93beb954649e16d613bee902ec3e
Quinnyy <michaelstvns2@gmail.com>
2016-12-05 22:00:47 +0000

    New home page/nav bar. Also base template for health page will finish it with danny tomorrow when I connect to the DB

---
+++ newIndex&Nav&Health_PAGES/index.php
@@ -0,0 +1,28 @@
+<?php
+include 'navBar.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<link href="master.css" rel="stylesheet" />
+
+<head>
+    <meta charset="UTF-8">
+    <title>Home Page</title>
+</head>
+<body>
+
+
+<div class="welcome">
+    <div class="visible">
+        <p>
+            Discover:
+        </p>
+        <ul>
+            <li> Clubs</li>
+            <li> Healthy Living</li>
+            <li> Local Trails Maps </li>
+        </ul>
+    </div>
+</div>
+</body>
---
+++ newIndex&Nav&Health_PAGES/navBar.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head runat = "server">
+    <title>
+        Discover Aberdeen
+    </title>
+
+
+    <link href="NavbarMaster.css" rel="stylesheet" />
+
+</head >
+
+<body class="master-body">
+<form id="form1" runat="server" style="height: 50px">
+    <header >
+        <nav>
+            <ul>
+                <li><a href="index.php"><b>Home</b></a></li>
+                <li>
+                    <a href="#"><b>Clubs</b></a>
+                </li>
+                <li><a href="healthyLiving.php"><b>Healthy Living</b></a></li>
+                <li><a href="#"><b>Local Trails</b></a></li>
+                <li><a href="#"><b>Site Users</b></a>
+                    <ul>
+                        <li><a href="#">Admin</a></li>
+                        <li><a href="contributorPage.php">Contributor</a></li>
+                    </ul>
+            </ul>
+        </nav>
+
+    </header>
+</form>
+</body>
+</html>
+
+<?php
+?>
