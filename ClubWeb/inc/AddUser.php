9a896975cd6013389901affa7ac0d508ba1976ee
1611267 <phaseiimusic@hotmail.com>
2016-12-05 14:07:55 +0000

    Registration page

--- ClubWeb/inc/AddUser.php
+++ ClubWeb/inc/AddUser.php
@@ -4,21 +4,6 @@
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    checkUsers($username, $db);
-
-    function checkUsers($username, $db) {
-        $sql = "SELECT username FROM port_users";
-        $result = mysqli_query($db, $sql);
-
-        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-            if($row['username'] == $username) {
-                return false;
-            }
-            //echo "<p>" . $row['username'] . "</p>";
-        }
-        return true;
-    }
-
     if(checkUsers($username, $db)) {
         $sql = "INSERT INTO port_users (username, password) VALUES ('$username', '$password')";
 
@@ -31,6 +16,19 @@
         mysqli_close($db);
     } else {
         echo "Sorry! That username is already in use.";
+    }
+
+    function checkUsers($username, $db) {
+        $sql = "SELECT username FROM port_users";
+        $result = mysqli_query($db, $sql);
+
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+            if($row['username'] == $username) {
+                return false;
+            }
+            //echo "<p>" . $row['username'] . "</p>";
+        }
+        return true;
     }
 
 ?>
