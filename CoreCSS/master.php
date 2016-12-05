f41f2c3bf535c49ace11ad5b5cbc39150e06ba6c
1608354 <a>
2016-12-05 13:30:00 +0000

    file changes

--- CoreCSS/master.php
+++ CoreCSS/master.php
@@ -21,8 +21,8 @@
     <header class="header">
         <nav>
             <ul>
-                <li><a href="../Maps/index.php"><b>Maps</b></a></li>
-                <li><a href="../newcss/clubs.html"><b>Clubs</b></a></li>
+                <li><a href="../Maps/Maps"><b>Maps</b></a></li>
+                <li><a href="ClubWeb"><b>Clubs</b></a></li>
                 <li><a href="../newcss/health.html"><b>Health</b></a></li>
              </ul>
 
