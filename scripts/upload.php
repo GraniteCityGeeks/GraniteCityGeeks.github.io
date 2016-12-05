f426a6cb0e7c7b19f14d733afb2a15576bccf853
1608354 <a>
2016-12-05 16:43:46 +0000

    uploads

--- scripts/upload.php
+++ scripts/upload.php
@@ -9,4 +9,9 @@
 if (isset($_FILES['file'])){
     $file = $_FILES['file'];
     print_r($file);
+    // store file properties
+    $file_name = $file['name'];
+    $file_tmp = $file['tmp_name'];
+    $file_size = $file['size'];
+    
 }
