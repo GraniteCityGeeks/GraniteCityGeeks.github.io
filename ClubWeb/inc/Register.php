85917fb76568c9b39d1b0d6183b1a39f7d9dbda2
JordanYoungman <jwyoungman96@homail.co.uk>
2016-12-05 15:45:43 +0000

    hi

--- ClubWeb/inc/Register.php
+++ ClubWeb/inc/Register.php
@@ -12,9 +12,9 @@
         <?
         include("scripts/Footer.php");
 
-            ini_set('display_errors', 1);
-            ini_set('display_startup_errors', 1);
-            error_reporting(E_ALL);
+            //ini_set('display_errors', 1);
+            //ini_set('display_startup_errors', 1);
+            //error_reporting(E_ALL);
 
             include("scripts/dbconnect.php");
 
