554d0df64bc2812eaecc699a5d9c329d19fec66f
JordanYoungman <jwyoungman96@homail.co.uk>
2016-12-05 14:32:02 +0000

    hi

--- ClubWeb/inc/Register.php
+++ ClubWeb/inc/Register.php
@@ -1,7 +1,7 @@
 <?php
-include("scripts/Header.php");
-?>
-
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    include("scripts/Header.php");
+    ?>
     <main>
         <form action="register" method="post">
             <input type="text" name="username" placeholder="username"></br>
@@ -10,19 +10,19 @@ include("scripts/Header.php");
         </form>
     </main>
 
-<?php
-include("scripts/Footer.php");
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+    <?php
+    include("scripts/Footer.php");
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 
     include("scripts/dbconnect.php");
 
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    if(checkUsers($username, $db)) {
+    if (checkUsers($username, $db)) {
         $sql = "INSERT INTO port_users (username, password) VALUES ('$username', '$password')";
 
         if (mysqli_query($db, $sql)) {
@@ -36,17 +36,19 @@ error_reporting(E_ALL);
         echo "Sorry! That username is already in use.";
     }
 
-    function checkUsers($username, $db) {
+    function checkUsers($username, $db)
+    {
         $sql = "SELECT username FROM port_users";
         $result = mysqli_query($db, $sql);
 
         while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-            if($row['username'] == $username) {
+            if ($row['username'] == $username) {
                 return false;
             }
             //echo "<p>" . $row['username'] . "</p>";
         }
         return true;
     }
+}
 
 ?>
