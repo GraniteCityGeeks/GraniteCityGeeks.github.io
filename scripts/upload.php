ccf834f05b578cca1e392b1f211fb2955db7b25a
Naelegs <jamiepeterlaw@gmail.com>
2016-12-05 17:57:20 +0000

    file upload

--- scripts/upload.php
+++ scripts/upload.php
@@ -13,5 +13,10 @@ if (isset($_FILES['file'])){
     $file_name = $file['name'];
     $file_tmp = $file['tmp_name'];
     $file_size = $file['size'];
+
+    $file_ext = explode('',$file_name);
+
+    print_r($file_ext);
+
     
 }
