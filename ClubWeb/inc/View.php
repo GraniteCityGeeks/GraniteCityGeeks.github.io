1ab633c3c1c7c2c3c439f0b77761f60bc3043102
JordanYoungman <jwyoungman96@homail.co.uk>
2016-12-06 12:52:40 +0000

    hi

--- ClubWeb/inc/View.php
+++ ClubWeb/inc/View.php
@@ -66,7 +66,7 @@ while ($row = $result->fetch_array()) {
     echo '<td>' . $row['bio'] . '</td>';
     echo '<td>' . $row['accessLevelID'] . '</td>';
     echo '<td>' . $row['photoID'] . '</td>';
-    echo '<td><a href="/edit" class="edit" >Edit</a></td>';
+    echo '<td><a href="edit" class="edit" >Edit</a></td>';
     echo '<td><a href="delete">Delete</a></td>';
     echo "</tr>";
 }
