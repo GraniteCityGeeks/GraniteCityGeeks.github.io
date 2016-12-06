31ba2a8aa6dfdbea0da1cc073c91f7693583aecc
1611267 <phaseiimusic@hotmail.com>
2016-12-06 14:36:53 +0000

    Registration page

--- ClubWeb/index.php
+++ ClubWeb/index.php
@@ -1,43 +1,43 @@
-<?//
-//define('INCLUDE_DIR', dirname(__FILE__) . '/inc/');
-//$rules = array(
-//    //
-//    //main pages
-//    //
-//    'about' => "/about",
-//    'contactus' => "/contactus",
-//    'blog' => "/blog",
-//    'blog_article' => "/blog/(?'blogID'[\w\-]+)",
-//    //
-//    //Admin Pages
-//    //
-//    'login' => "/login",
-//    'create_article' => "/createarticle",
-//    'view' => "/view",
-//    'logout' => "/logout",
-//    'register' => "/register",
-//    'add' => "/add",
-//    'edit' => "/edit",
-//    'delete' => "/delete",
-//    'AddUser' => "/AddUser",
-//    'Clubs' => "/Clubs",
-//    //
-//    // Home Page
-//    //
-//    'home' => "/"
-//    //
-//    // Style
-//    //
-//);
-//$uri = rtrim(dirname($_SERVER["SCRIPT_NAME"]), '/');
-//$uri = '/' . trim(str_replace($uri, '', $_SERVER['REQUEST_URI']), '/');
-//$uri = urldecode($uri);
-//foreach ($rules as $action => $rule) {
-//   if (preg_match('~^' . $rule . '$~i', $uri, $params)) {
-//       include(INCLUDE_DIR . $action . '.php');
-//       exit();
-//   }
-//}
-//// nothing is found so handle the 404 error
-//include(INCLUDE_DIR . '404.php');
-//?>
+<?
+define('INCLUDE_DIR', dirname(__FILE__) . '/inc/');
+$rules = array(
+    //
+    //main pages
+    //
+    'about' => "/about",
+    'contactus' => "/contactus",
+    'blog' => "/blog",
+    'blog_article' => "/blog/(?'blogID'[\w\-]+)",
+    //
+    //Admin Pages
+    //
+    'login' => "/login",
+    'create_article' => "/createarticle",
+    'view' => "/view",
+    'logout' => "/logout",
+    'register' => "/register",
+    'add' => "/add",
+    'edit' => "/edit",
+    'delete' => "/delete",
+    'AddUser' => "/AddUser",
+    'Clubs' => "/Clubs",
+    //
+    // Home Page
+    //
+    'home' => "/"
+    //
+    // Style
+    //
+);
+$uri = rtrim(dirname($_SERVER["SCRIPT_NAME"]), '/');
+$uri = '/' . trim(str_replace($uri, '', $_SERVER['REQUEST_URI']), '/');
+$uri = urldecode($uri);
+foreach ($rules as $action => $rule) {
+   if (preg_match('~^' . $rule . '$~i', $uri, $params)) {
+       include(INCLUDE_DIR . $action . '.php');
+       exit();
+   }
+}
+// nothing is found so handle the 404 error
+include(INCLUDE_DIR . '404.php');
+?>
