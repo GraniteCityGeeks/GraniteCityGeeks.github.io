283d136357db212ce685b112783ec8832e89ca45
1611267 <phaseiimusic@hotmail.com>
2016-12-06 13:45:56 +0000

    Registration page

--- ClubWeb/inc/Edit.php
+++ ClubWeb/inc/Edit.php
@@ -1,214 +1,63 @@
 <?php
+include("scripts/header.php");
+?>
+    <main>
+        <form action="edit" method="post">
+            <input type="text" name="username" placeholder="username"></br>
+            <input type="password" name="password" placeholder="password"></br>
+            <input type="text" name="bio" placeholder="bio"></br>
+            <input type="radio" name="accessLevelID" value= "2" > Contributor<br>
+            <input type="radio" name="accessLevelID" value= "3"> NKPAG<br>
+            <input type="radio" name="accessLevelID" value= "4"> Club Administrator<br>
+            <input type="radio" name="accessLevelID" value= "5"> Site Administrator<br>
+            <p><input type="submit" value="Submit"></p>
+        </form>
+    </main>
 
-function renderForm($id, $username, $password, $error)
+<?
+include("scripts/Footer.php");
 
-{
-
-    ?>
-
-    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-
-    <html>
-
-    <head>
-
-        <title>Edit Record</title>
-
-    </head>
-
-    <body>
-
-    <?php
-
-    // if there are any errors, display them
-
-    if ($error != '')
-
-    {
-
-        echo '<div style="padding:4px; border:1px solid red; color:red;">'.$error.'</div>';
-
-    }
-
-    ?>
-
-
-    <form action="" method="post">
-
-        <input type="hidden" name="id" value="<?php echo $id; ?>"/>
-
-        <div>
-
-            <p><strong>ID:</strong> <?php echo $id; ?></p>
-
-            <strong>First Name: *</strong> <input type="text" name="username" value="<?php echo $username; ?>"/><br/>
-
-            <strong>Last Name: *</strong> <input type="text" name="password" value="<?php echo $password; ?>"/><br/>
-
-            <p>* Required</p>
-
-            <input type="submit" name="submit" value="Submit">
-
-        </div>
-
-    </form>
-
-    </body>
-
-    </html>
-
-    <?php
-
-}
-
-// connect to the database
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 include("scripts/dbconnect.php");
 
+$username = $_POST['username'];
+$password = $_POST['password'];
+$bio = $_POST['bio'];
+$accessLevelID = $_POST['accessLevelID'];
 
+if (checkUsers($username, $db)) {
+    $sql = "INSERT INTO port_users (username, password,bio,accessLevelID) VALUES ('$username', '$password','$bio', '$accessLevelID')";
 
-// check if the form has been submitted. If it has, process the form and save it to the database
+    if (mysqli_query($db, $sql)) {
+        echo "New record created succesfully";
 
-if (isset($_POST['submit']))
-
-{
-
-// confirm that the 'id' value is a valid integer before getting the form data
-
-    if (is_numeric($_POST['id']))
-
-    {
-
-// get form data, making sure it is valid
-
-        $id = $_POST['id'];
-
-        $username = mysqli_real_escape_string(htmlspecialchars($_POST['firstname']));
-
-        $password = mysqli_real_escape_string(htmlspecialchars($_POST['lastname']));
-
-
-
-// check that firstname/lastname fields are both filled in
-
-        if ($username == '' || $password == '')
-
-        {
-
-// generate error message
-
-            $error = 'ERROR: Please fill in all required fields!';
-
-
-
-//error, display form
-
-            renderForm($id, $username, $password, $error);
-
-        }
-
-        else
-
-        {
-
-// save the data to the database
-
-            mysqli_query("UPDATE port_users SET firstname='$firstname', lastname='$lastname' WHERE id='$id'")
-
-            or die(mysqli_error());
-
-
-
-// once saved, redirect back to the view page
-
-            header("Location: /view");
-
-        }
-
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($db);
     }
 
-    else
-
-    {
-
-// if the 'id' isn't valid, display an error
-
-        echo 'Error!';
-
-    }
-
+    mysqli_close($db);
+} elseif ($username == '' || $password == ''){
+    echo "Please enter a username and password";
+}
+else{
+    echo "User already exists";
 }
 
-else
+function checkUsers($username, $db){
+    $sql = "SELECT username FROM port_users";
+    $result = mysqli_query($db, $sql);
 
-// if the form hasn't been submitted, get the data from the db and display the form
-
-{
-
-
-
-// get the 'id' value from the URL (if it exists), making sure that it is valid (checking that it is numeric/larger than 0)
-
-    if (isset($_GET['id']) && is_numeric($_GET['id']) && $_GET['id'] > 0)
-
-    {
-
-// query db
-
-        $id = $_GET['id'];
-
-        $result = mysqli_query("SELECT * FROM players WHERE id=$id")
-
-        or die(mysqli_error());
-
-        $row = mysqli_fetch_array($result);
-
-
-
-// check that the 'id' matches up with a row in the databse
-
-        if($row)
-
-        {
-
-
-
-// get data from db
-
-            $username = $row['username'];
-
-            $password = $row['password'];
-
-
-
-// show form
-
-            renderForm($id, $username, $password, '');
-
+    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+        if ($row['username'] == $username) {
+            return false;
         }
-
-        else
-
-// if no match, display result
-
-        {
-
-            echo "No results!";
-
-        }
-
     }
-
-    else
-
-// if the 'id' in the URL isn't valid, or if there is no 'id' value, display an error
-
-    {
-
-        echo 'Error!';
-
-    }
-
+    return true;
 }
 
-?>
+function loadData($db) {
+    if(isset($_POST['']))
+}
