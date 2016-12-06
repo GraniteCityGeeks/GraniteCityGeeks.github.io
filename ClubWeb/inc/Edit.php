1cda8b87bbd841bef842a3163eaab377153a7388
1611267 <phaseiimusic@hotmail.com>
2016-12-06 14:19:58 +0000

    Registration page

--- ClubWeb/inc/Edit.php
+++ ClubWeb/inc/Edit.php
@@ -2,6 +2,7 @@
 //$ID = $_POST['var'];
 //echo 'ID ' . $ID .';';
 //echo 'hello world';
+include("scripts/dbconnect.php");
 include("scripts/header.php");
 loadData($db);
 
@@ -25,42 +26,42 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-include("scripts/dbconnect.php");
 
-$username = $_POST['username'];
-$password = $_POST['password'];
-$bio = $_POST['bio'];
-$accessLevelID = $_POST['accessLevelID'];
 
-if (checkUsers($username, $db)) {
-    $sql = "INSERT INTO port_users (username, password,bio,accessLevelID) VALUES ('$username', '$password','$bio', '$accessLevelID')";
-
-    if (mysqli_query($db, $sql)) {
-        echo "New record created succesfully";
-
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($db);
-    }
-
-    mysqli_close($db);
-} elseif ($username == '' || $password == ''){
-    echo "Please enter a username and password";
-}
-else{
-    echo "User already exists";
-}
-
-function checkUsers($username, $db){
-    $sql = "SELECT username FROM port_users";
-    $result = mysqli_query($db, $sql);
-
-    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-        if ($row['username'] == $username) {
-            return false;
-        }
-    }
-    return true;
-}
+//$username = $_POST['username'];
+//$password = $_POST['password'];
+//$bio = $_POST['bio'];
+//$accessLevelID = $_POST['accessLevelID'];
+//
+//if (checkUsers($username, $db)) {
+//    $sql = "INSERT INTO port_users (username, password,bio,accessLevelID) VALUES ('$username', '$password','$bio', '$accessLevelID')";
+//
+//    if (mysqli_query($db, $sql)) {
+//        echo "New record created succesfully";
+//
+//    } else {
+//        echo "Error: " . $sql . "<br>" . mysqli_error($db);
+//    }
+//
+//    mysqli_close($db);
+//} elseif ($username == '' || $password == ''){
+//    echo "Please enter a username and password";
+//}
+//else{
+//    echo "User already exists";
+//}
+//
+//function checkUsers($username, $db){
+//    $sql = "SELECT username FROM port_users";
+//    $result = mysqli_query($db, $sql);
+//
+//    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+//        if ($row['username'] == $username) {
+//            return false;
+//        }
+//    }
+//    return true;
+//}
 
 function loadData($db) {
     $userID = $_POST['var'];
