f420b244868eca583c7b1d95c974a4b2c6201cb6
JordanYoungman <jwyoungman96@homail.co.uk>
2016-12-06 13:05:15 +0000

    hi

--- ClubWeb/inc/Add.php
+++ ClubWeb/inc/Add.php
@@ -17,9 +17,9 @@ include("scripts/header.php");
 <?
 include("scripts/Footer.php");
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
 
 include("scripts/dbconnect.php");
 
