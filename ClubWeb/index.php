243909c88e28737cf6072d11378ec37c2c033276
JordanYoungman <jwyoungman96@homail.co.uk>
2016-12-06 14:13:36 +0000

    hi

--- ClubWeb/index.php
+++ ClubWeb/index.php
@@ -32,12 +32,12 @@ $rules = array(
 $uri = rtrim(dirname($_SERVER["SCRIPT_NAME"]), '/');
 $uri = '/' . trim(str_replace($uri, '', $_SERVER['REQUEST_URI']), '/');
 $uri = urldecode($uri);
-foreach ($rules as $action => $rule) {
-    if (preg_match('~^' . $rule . '$~i', $uri, $params)) {
-        include(INCLUDE_DIR . $action . '.php');
-        exit();
-    }
-}
+//foreach ($rules as $action => $rule) {
+//    if (preg_match('~^' . $rule . '$~i', $uri, $params)) {
+//        include(INCLUDE_DIR . $action . '.php');
+//        exit();
+//    }
+//}
 // nothing is found so handle the 404 error
 include(INCLUDE_DIR . '404.php');
 ?>
