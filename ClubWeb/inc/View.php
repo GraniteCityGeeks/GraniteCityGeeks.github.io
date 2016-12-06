84f0f3a2e3f8d13484c8cbd2b1b47765cea524c7
JordanYoungman <jwyoungman96@homail.co.uk>
2016-12-06 12:46:51 +0000

    hi

--- ClubWeb/inc/View.php
+++ ClubWeb/inc/View.php
@@ -22,6 +22,32 @@ th {
 }
 
 tr:nth-child(even){background-color: #f2f2f2}
+
+.edit {
+	-moz-box-shadow: 0px 0px 0px 0px #3dc21b;
+	-webkit-box-shadow: 0px 0px 0px 0px #3dc21b;
+	box-shadow: 0px 0px 0px 0px #3dc21b;
+	background-color:#44c767;
+	-moz-border-radius:14px;
+	-webkit-border-radius:14px;
+	border-radius:14px;
+	border:1px solid #18ab29;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:21px;
+	padding:9px 18px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #2f6627;
+}
+.edit:hover {
+	background-color:#5cbf2a;
+}
+.edit:active {
+	position:relative;
+	top:1px;
+}
 </style>";
 
 echo "<p><b>View All</b></p>";
@@ -41,8 +67,8 @@ while ($row = $result->fetch_array()) {
     echo '<td>' . $row['bio'] . '</td>';
     echo '<td>' . $row['accessLevelID'] . '</td>';
     echo '<td>' . $row['photoID'] . '</td>';
-    echo '<td><form action="edit"><input type="submit" value="Edit" /></form></td>';
-    echo '<td><form action="delete"><input type="submit" value="Delete" /></form></td>';
+    echo '<p><a href="edit" class="edit" >Edit</a></p>';
+    echo '<p><a href="delete">Delete</a></p>';
     echo "</tr>";
 }
 
