3568516260597fa164631f361343bf367208a612
JordanYoungman <jwyoungman96@homail.co.uk>
2016-12-06 13:29:44 +0000

    hi

--- ClubWeb/inc/View.php
+++ ClubWeb/inc/View.php
@@ -92,7 +92,7 @@ while ($row = $result->fetch_array()) {
     echo '<td>' . $row['accessLevelID'] . '</td>';
     echo '<td>' . $row['photoID'] . '</td>';
     echo '<td><a href="edit" class="edit' . $row['userID']. '">Edit</a></td>';
-    echo '<td><a href="delete.php?id=' . $row['userID']. '">Delete!!</a></td>';
+    echo '<td><a href="delete?id=' . $row['userID']. '">Delete!!</a></td>';
     echo "</tr>";
 }
 
