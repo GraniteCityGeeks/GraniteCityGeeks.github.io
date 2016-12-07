944c44d9359f3be6424cdc19007fe94a8ce39fd1
JordanYoungman <jwyoungman96@homail.co.uk>
2016-12-07 11:18:42 +0000

    hi

--- ClubWeb/inc/Home.php
+++ ClubWeb/inc/Home.php
@@ -5,7 +5,6 @@ session_start();
 echo "
 <main>
 <p>Welcome {$_SESSION['username']}! In this blog you will {$_SESSION['accessLevelID']} see all of my insights and wonderful things yo {$_SESSION['photoID']}</p>
-<p><img src= alt=\"Mountain View\" style=\"width:304px;height:228px;\"></p>
 </main>
 ";
 include ("scripts/footer.php");
