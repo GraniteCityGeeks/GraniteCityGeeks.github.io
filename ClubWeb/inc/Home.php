ac42031058a3f8b4edcbff34ec48d5543a007b69
JordanYoungman <jwyoungman96@homail.co.uk>
2016-12-07 12:19:21 +0000

    hi

--- ClubWeb/inc/Home.php
+++ ClubWeb/inc/Home.php
@@ -1,11 +1,18 @@
 <?php
 include ("scripts/header.php");
 session_start();
-
-echo "
+if (isset($_SESSION['username'])) {
+    echo "
 <main>
 <p>Welcome {$_SESSION['username']}! You have an access level of: {$_SESSION['accessLevelID']} your userID is: {$_SESSION['userID']} and Profile pic is: <img src={$_SESSION['photoID']} alt=\"Mountain View\" style=\"width:304px;height:228px;\"></p>
 </main>
 ";
+} else {
+    echo "
+<main>
+<p>Welcome to the clubs page.</p>
+</main>
+";
+}
 include ("scripts/footer.php");
 ?>
