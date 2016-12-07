fc6e7e02b53af0864344cd23fdcc3f5fdf115e4a
Quinnyy <michaelstvns2@gmail.com>
2016-12-07 19:59:09 +0000

    Add files via upload

---
+++ health/sourceFiles/master.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head runat = "server">
+    <title>
+        Discover Aberdeen
+    </title>
+
+
+    <link href="NavbarMaster.css" rel="stylesheet" />
+
+</head >
+
+<body class="master-body">
+<form id="form1" runat="server" style="height: 50px">
+    <header >
+        <nav>
+            <ul>
+                <li><a href="index.php"><b>Home</b></a></li>
+                <li>
+                    <a href="#"><b>Clubs</b></a>
+                </li>
+                <li><a href="healthyLiving.php"><b>Healthy Living</b></a></li>
+                <li><a href="#"><b>Local Trails</b></a></li>
+                <li><a href="#"><b>Site Users</b></a>
+                    <ul>
+                        <li><a href="#">Admin</a>
+                        <ul>
+                            <li><a href="#">Clubs</a></li>
+                            <li><a href="#">Health</a></li>
+                            <li><a href="#">Maps</a></li>
+                        </ul></li>
+                        <li><a href="#">Contributor</a>
+                        <ul>
+                            <li><a href="#">Clubs</a></li>
+                            <li><a href="contributorPage.php">Health</a></li>
+                            <li><a href="#">Maps</a></li>
+
+                        </ul>
+
+                     </ul></li>
+
+            </ul>
+        </nav>
+
+    </header>
+</form>
+</body>
+</html>
+
+<?php
+?>
