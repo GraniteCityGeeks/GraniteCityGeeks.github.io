d709bb59167e20f89ea62c61a44b484c7abf00e7
JordanYoungman <jwyoungman96@homail.co.uk>
2016-12-07 11:08:37 +0000

    hi

--- ClubWeb/inc/Home.php
+++ ClubWeb/inc/Home.php
@@ -5,7 +5,7 @@ session_start();
 $photo = $_SESSION['photoID'];
 echo "
 <main>
-<p>Welcome {$_SESSION['username']}! In this blog you will {$_SESSION['accessLevelID']} see all of my insights and wonderful things </p>
+<p>Welcome {$_SESSION['username']}! In this blog you will {$_SESSION['accessLevelID']} see all of my insights and wonderful things yo</p>
 <p><img src=[$photo} alt=\"Mountain View\" style=\"width:304px;height:228px;\"></p>
 </main>
 ";
