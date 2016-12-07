2f36896ddf2f53327d3cf35bfc990ae90ac38d18
JordanYoungman <jwyoungman96@homail.co.uk>
2016-12-07 10:44:50 +0000

    hi

--- ClubWeb/inc/Delete.php
+++ ClubWeb/inc/Delete.php
@@ -11,14 +11,14 @@ if (isset($_GET['userID']) && is_numeric($_GET['userID']))
     $sql = "DELETE FROM port_users WHERE userID=$userID";
     $result = $db->query($sql);
 
-    header("Location: /view");
+    header("Location: /View");
 
 }
 
 else
 
 {
-    header("Location: /view");
+    header("Location: /View");
 }
 
 
