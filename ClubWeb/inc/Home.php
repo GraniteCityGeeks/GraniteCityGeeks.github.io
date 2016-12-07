d98908129e49f855a4de438df3ba1d2e5d1f0926
JordanYoungman <jwyoungman96@homail.co.uk>
2016-12-07 12:03:55 +0000

    hi

--- ClubWeb/inc/Home.php
+++ ClubWeb/inc/Home.php
@@ -4,7 +4,7 @@ session_start();
 
 echo "
 <main>
-<p>Welcome {$_SESSION['username']}! You have an access level of: {$_SESSION['accessLevelID']} your userID is: {$_SESSION['userID']} and Profile pic is: {$_SESSION['photoID']}</p>
+<p>Welcome {$_SESSION['username']}! You have an access level of: {$_SESSION['accessLevelID']} your userID is: {$_SESSION['userID']} and Profile pic is: <img src={$_SESSION['photoID']} alt=\"Mountain View\" style=\"width:304px;height:228px;\"></p>
 </main>
 ";
 include ("scripts/footer.php");
