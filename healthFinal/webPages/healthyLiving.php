7e3e02588cae5167b5713dfd1602f5b8a3a63724
Quinnyy <michaelstvns2@gmail.com>
2016-12-08 12:52:55 +0000

    Health Pages

--- healthFinal/webPages/healthyLiving.php
+++ healthFinal/webPages/healthyLiving.php
@@ -41,7 +41,7 @@ include '../navBar/master.php';
             <i>This page will contain helpful information, tips & tricks for staying healthy!</i>
         </div>
     </div>
-    <hr />
+    <hr class="style-three" />
 
         <h2>News Feed!</h2>
 
