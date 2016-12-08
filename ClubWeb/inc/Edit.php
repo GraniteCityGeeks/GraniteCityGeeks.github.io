b88955bd57dffc2722f5a3ad35439600de2f8495
Jordan <jwyoungman96@hotmail.co.uk>
2016-12-08 04:40:38 +0000

    test

--- ClubWeb/inc/Edit.php
+++ ClubWeb/inc/Edit.php
@@ -2,21 +2,6 @@
 include("scripts/header.php");
 function renderForm($id, $username, $password, $error){
 
-    ?>
-
-    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-
-    <html>
-
-    <head>
-
-        <title>Edit Record</title>
-
-    </head>
-
-    <body>
-
-    <?php
     if ($error != '')
 
     {
@@ -26,30 +11,16 @@ function renderForm($id, $username, $password, $error){
     }
 
     ?>
-
+    <main>
     <form action="" method="post">
-
         <input type="hidden" name="id" value="<?php echo $id; ?>"/>
-
         <div>
-
             <p><strong>ID:</strong> <?php echo $id; ?></p>
-
             <strong>First Name: *</strong> <input type="text" name="username" value="<?php echo $username; ?>"/><br/>
-
             <strong>Last Name: *</strong> <input type="text" name="password" value="<?php echo $password; ?>"/><br/>
-
             <p>* Required</p>
-
             <input type="submit" name="submit" value="Submit">
-
-        </div>
-
-    </form>
-
-    </body>
-
-    </html>
+    </main>
 
     <?php
 
