41ed41f52920f0095ed8d99af15cc27af91a7616
Naelegs <jamiepeterlaw@gmail.com>
2016-12-08 22:53:33 +0000

    css

--- Maps/index.php
+++ Maps/index.php
@@ -1,6 +1,5 @@
 <?php
 include'../healthFinal/navBar/master.php'
-
 ?>
 
 <html>
@@ -8,94 +7,78 @@ include'../healthFinal/navBar/master.php'
 <link href="bootstrap.min.css" rel="stylesheet" />
 <link href="/healthFinal/CSS/half-slider.css" rel="stylesheet" />
 <link href="/CoreCSS/master.css" rel="stylesheet" />
-<link href="/healthFinal/CSS/healthPage.css">
+
 <head>
 
-<title>GoPortlethen</title>
-<style>
-    @import url('https://fonts.googleapis.com/css?family=Roboto');
-</style>
+    <title>GoPortlethen</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=Roboto');
+    </style>
 
 
-<script type="text/javascript">
-    function initMap() {
-        map = new google.maps.Map(document.getElementById('initmap'), {
-            center: {lat: 57.061681, lng: -2.129468},
-            zoom: 12,
-            MapTypeId: "satellite"
-        });
-    }
-</script>
-<script type="text/javascript">
-    //Get the markers from the XML document.
-
-    function downloadUrl(url, callback) {
-        var request = window.ActiveXObject ?
-            new ActiveXObject('Microsoft.XMLHTTP') :
-            new XMLHttpRequest;
-
-        request.onreadystatechange = function () {
-            if (request.readyState == 4) {
-                callback(request, request.status);
-            }
-        };
-
-        request.open('GET', url, true);
-        request.send(null);
-    }
-
-    //download the locations.
-
-    downloadUrl("../scripts/markers.php", function (data) {
-        var xml = data.responseXML;
-
-        var markers = xml.documentElement.getElementsByTagName("markers");
-        for (var i = 0; i < markers.length; i++) {
-            var desc = markers[i].getAttribute("description");
-            var name = markers[i].getAttribute("name");
-            var point = new google.maps.LatLng(
-
-                parseFloat(markers[i].getAttribute("lat")),
-                parseFloat(markers[i].getAttribute("lng"))
-            );
-
-            addMarker(i, point, desc, name);
-
+    <script type="text/javascript">
+        function initMap() {
+            map = new google.maps.Map(document.getElementById('initmap'), {
+                center: {lat: 57.061681, lng: -2.129468},
+                zoom: 12,
+                MapTypeId: "satellite"
+            });
         }
-    });
-
-    var markersArray = [];  //array for markers
-
-    //this fucntion will add the markers.
-    function addMarker(id,point,description,name) {
-        id = new google.maps.Marker({
-            map: map,
-            draggable: false,
-            animation: google.maps.Animation.DROP,
-            position: point
-
+    </script>
+    <script type="text/javascript">
+        //Get the markers from the XML document.
+        function downloadUrl(url, callback) {
+            var request = window.ActiveXObject ?
+                new ActiveXObject('Microsoft.XMLHTTP') :
+                new XMLHttpRequest;
+            request.onreadystatechange = function () {
+                if (request.readyState == 4) {
+                    callback(request, request.status);
+                }
+            };
+            request.open('GET', url, true);
+            request.send(null);
+        }
+        //download the locations.
+        downloadUrl("../scripts/markers.php", function (data) {
+            var xml = data.responseXML;
+            var markers = xml.documentElement.getElementsByTagName("markers");
+            for (var i = 0; i < markers.length; i++) {
+                var desc = markers[i].getAttribute("description");
+                var name = markers[i].getAttribute("name");
+                var point = new google.maps.LatLng(
+                    parseFloat(markers[i].getAttribute("lat")),
+                    parseFloat(markers[i].getAttribute("lng"))
+                );
+                addMarker(i, point, desc, name);
+            }
         });
-        var contentstring = '<div id="content">' +
-            '<h1>'+ name +'</h1>'+
-            '<p>'+ description +'</p>';
-        var infowindow = new google.maps.InfoWindow({
-            content:contentstring
-        });
-
-        id.addListener('click', function() {
-            map.setZoom(15);
-            map.setCenter(id.getPosition());
-            infowindow.open(map, id);
-
-        });
-
-        markersArray.push(id);
-    }
-
-</script>
-<script async defer
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCC8HwZx1Aknt-BHgT2vYtcgeBBvokVzWU&callback=initMap">
-</script>
+        var markersArray = [];  //array for markers
+        //this fucntion will add the markers.
+        function addMarker(id,point,description,name) {
+            id = new google.maps.Marker({
+                map: map,
+                draggable: false,
+                animation: google.maps.Animation.DROP,
+                position: point
+            });
+            var contentstring = '<div id="content">' +
+                '<h1>'+ name +'</h1>'+
+                '<p>'+ description +'</p>';
+            var infowindow = new google.maps.InfoWindow({
+                content:contentstring
+            });
+            id.addListener('click', function() {
+                map.setZoom(15);
+                map.setCenter(id.getPosition());
+                infowindow.open(map, id);
+            });
+            markersArray.push(id);
+        }
+    </script>
+    <script async defer
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCC8HwZx1Aknt-BHgT2vYtcgeBBvokVzWU&callback=initMap">
+    </script>
 
     <meta charset="UTF-8">
     <title></title>
@@ -107,11 +90,11 @@ include'../healthFinal/navBar/master.php'
             <div class="fill" style="background-image: url('gold.jpg')"></div>
         </div>
         <div class="item">
-            <div class="fill" style="background-image: url('ASPC_ABERDEEN-IN-SNOW.jpg')"></div>
+            <div class="fill" style="background-image: url('train.jpg')"></div>
 
         </div>
         <div class="item">
-            <div class="fill" style="background-image: url('rem.jpg')"></div>
+            <div class="fill" style="background-image: url('castle.jpg')"></div>
 
         </div>
     </div>
@@ -134,21 +117,14 @@ include'../healthFinal/navBar/master.php'
             <i>This page will show points of interest on an interactive map!</i>
         </div>
 
+        <hr class="type_3" />
 
-    <hr class="type_3" />
-        <br>
-        <br>
-        <br>
-        <br>
+        <div id="map">
+
+        </div>
+
+
         <div class="col-lg-12" style="width: 350px">
-        <div id="top" style="float:left position:absolute;">
-
-            <div id="initmap" style="height:700px;width:1250px; float:left;">
-            </div>
-    </div>
-</div>
-
-            <div class="col-lg-12" style="width: 350px">
             <div id="twitter">
                 <a class="twitter-timeline" data-width="400" data-height="800" d href="https://twitter.com/PortlethenGC">Tweets by PortlethenGC</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
             </div>
@@ -176,14 +152,18 @@ include'../healthFinal/navBar/master.php'
 <br>
 <br>
 
+<div id="top" style="float:left position:absolute;">
+
+    <div id="initmap" style="height:700px;width:1250px; float:left;">
+    </div>
 
 
 
-<br>
-<br>
-<br>
-<br>
-<br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
 
 
 </html>
