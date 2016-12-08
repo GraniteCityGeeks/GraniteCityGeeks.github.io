bcf4ba82f43c8d1aede0a1253847a24fb2aef760
Jordan <jwyoungman96@hotmail.co.uk>
2016-12-08 05:44:05 +0000

    test

--- ClubWeb/inc/Edit.php
+++ ClubWeb/inc/Edit.php
@@ -42,7 +42,7 @@ if (isset($_POST['submit']))
         $password = $_POST['password'];
         $bio = $_POST['bio'];
         $photoID = $_POST['photoID'];
-        $accessLevelID = $_POST['acessLevelID'];
+        $accessLevelID = $_POST['accessLevelID'];
 
 
         if ($username == '' || $password == '') {
