9edf573456022cef497de43e0dd0439fa6040f5a
Jordan <jwyoungman96@hotmail.co.uk>
2016-12-08 05:41:13 +0000

    test

--- ClubWeb/inc/Edit.php
+++ ClubWeb/inc/Edit.php
@@ -2,9 +2,9 @@
 include("scripts/header.php");
 
 
-//ini_set('display_errors', 1);
-//ini_set('display_startup_errors', 1);
-//error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 function renderForm($id, $username, $password,$bio,$photoID,$accessLevelID, $db){
 
