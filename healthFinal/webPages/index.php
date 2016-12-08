7d0ba340e836d94b9e58d4db01098a7572d31138
Quinnyy <michaelstvns2@gmail.com>
2016-12-08 20:31:30 +0000

    ch-ch-changes!

---
+++ healthFinal/webPages/index.php
@@ -0,0 +1,29 @@
+<?php
+include '../navBar/master.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<link href="../CSS/master.css" rel="stylesheet" />
+
+<head>
+    <meta charset="UTF-8">
+    <title>Home Page</title>
+</head>
+<body>
+
+
+<div class="welcome">
+    <div class="visible">
+        <p>
+Discover:
+        </p>
+        <ul>
+            <li> Clubs</li>
+            <li> Healthy Living</li>
+            <li> Local Trails Maps </li>
+        </ul>
+    </div>
+</div>
+</body>
+</html>
