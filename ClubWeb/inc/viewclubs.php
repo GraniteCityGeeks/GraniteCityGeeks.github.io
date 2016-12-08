012fb5ac71140d2279b077adb9bbda335559743e
John Doe <murraylyne2@Gmail.com>
2016-12-08 16:19:00 +0000

    viewclubs

--- ClubWeb/inc/viewclubs.php
+++ ClubWeb/inc/viewclubs.php
@@ -21,7 +21,7 @@ while($row = $result->fetch_array()) {
     //echo all clubs
     echo "<form action='Clubs' method='get'>";
     echo"<img src='".$row['URL']. "height='300', width='300'>";
-    echo"<h1><a href='Clubs' name='linkref' value='".$row['clubid']."'>".$row['clubTitle']."</a></h1>";
+    echo"<h1><a href='Clubs' name='linkref' onclick=\"document.forms['name_of_your_form'].submit(); value='".$row['clubid']."'>".$row['clubTitle']."</a></h1>";
     echo"<br>";
     echo"</form>";
 
