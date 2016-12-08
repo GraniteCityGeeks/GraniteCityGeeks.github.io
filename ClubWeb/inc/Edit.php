77dd5e008968a6a1545253c8ff05e24fa2737728
Jordan <jwyoungman96@hotmail.co.uk>
2016-12-08 04:26:39 +0000

    test

--- ClubWeb/inc/Edit.php
+++ ClubWeb/inc/Edit.php
@@ -48,77 +48,77 @@ function renderForm($id, $username, $password, $error)
 
 }
 
-//include('scripts/dbconnect.php');
-//
-//if (isset($_POST['submit']))
-//
-//{
-//
-//    if (is_numeric($_POST['id'])) {
-//
-//        $id = $_POST['id'];
-//
-//        $username = $_POST['username'];
-//
-//        $password = $_POST['password'];
-//
-//
-//        if ($username == '' || $password == '') {
-//            $error = 'ERROR: Please fill in all required fields!';
-//
-//            renderForm($id, $username, $password, $error);
-//
-//        } else {
-//
-//            mysqli_query("UPDATE port_users SET username='$username', password='$password' WHERE id='$id'");
-//
-//            header("Location: /ClubWeb/View");
-//        }
-//
-//    } else {
-//
-//        echo 'Error!';
-//
-//    }
-//
-//} else {
-//
-//
-//
-//    $ID = $params['userID'];
-//
-//    if (isset($params['userID']) && is_numeric($params['userID']) && ($params['userID']) > 0)
-//
-//    {
-//
-//// query db
-//
-//        $id = ($params['userID']);
-//
-//        $result = "SELECT * FROM port_users WHERE id=$id";
-//
-//        $row = $result->fetch_array();
-//
-//        if($row) {
-//
-//            $username = $row['username'];
-//
-//            $password = $row['password'];
-//
-//            renderForm($id, $username, $password, '');
-//
-//        } else {
-//
-//            echo "No results!";
-//
-//        }
-//
-//    } else {
-//
-//        echo 'Error!';
-//
-//    }
-//
-//}
+include('scripts/dbconnect.php');
+
+if (isset($_POST['submit']))
+
+{
+
+    if (is_numeric($_POST['id'])) {
+
+        $id = $_POST['id'];
+
+        $username = $_POST['username'];
+
+        $password = $_POST['password'];
+
+
+        if ($username == '' || $password == '') {
+            $error = 'ERROR: Please fill in all required fields!';
+
+            renderForm($id, $username, $password, $error);
+
+        } else {
+
+            mysqli_query("UPDATE port_users SET username='$username', password='$password' WHERE id='$id'");
+
+            header("Location: /ClubWeb/View");
+        }
+
+    } else {
+
+        echo 'Error!';
+
+    }
+
+} else {
+
+
+
+    $id = $params['userID'];
+
+    if (isset($params['userID']) && is_numeric($params['userID']) && ($params['userID']) > 0)
+
+    {
+
+// query db
+
+        $id = ($params['userID']);
+
+        $result = "SELECT * FROM port_users WHERE id=$id";
+
+        $row = $result->fetch_array();
+
+        if($row) {
+
+            $username = $row['username'];
+
+            $password = $row['password'];
+
+            renderForm($id, $username, $password, '');
+
+        } else {
+
+            echo "No results!";
+
+        }
+
+    } else {
+
+        echo 'Error!';
+
+    }
+
+}
 
 ?>
