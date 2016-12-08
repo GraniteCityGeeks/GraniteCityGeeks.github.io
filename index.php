ec0919cb0dc13b3fa4a2fe678d736f671520fc57
Naelegs <jamiepeterlaw@gmail.com>
2016-12-08 15:37:45 +0000

    php

--- index.php
+++ index.php
@@ -4,10 +4,11 @@ include 'master.php';
 
 <!DOCTYPE html>
 <html lang="en">
-
+<!--
 <link href="../CSS/bootstrap.min.css" rel="stylesheet" />
 <link href="../CSS/half-slider.css" rel="stylesheet" />
 <link href="../CSS/healthPage.css" rel="stylesheet" />
+-->
 
 
 <h1> Granite City Geeks - Go Portlethen</h1>
