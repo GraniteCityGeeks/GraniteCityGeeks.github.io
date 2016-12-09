4004c0863687ee81adc23521bcd5a64a8cc37f31
Medscootsman <murraylyne2@Gmail.com>
2016-12-09 14:47:46 +0000

    brought the map back to before it was fucked up

--- ClubWeb/inc/createclub.php
+++ ClubWeb/inc/createclub.php
@@ -3,30 +3,30 @@ session_start();
 if (isset($_SESSION['username'])) //SESSION DOES EXIST
 {
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        include ("scripts/header_l2.php.php");
-echo "
+        include("scripts/header_l2.php.php");
+        echo "
 <main>
 ";
-?>
-// This will be used to make/edit club pages.
-    <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+        ?>
+        // This will be used to make/edit club pages.
+        <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
         <script>tinymce.init({selector: 'textarea'});</script>
-            <form action="createclub" method="post">
-                <input type="text" name="clubTitle" placeholder="Club's Name">
-                <textarea name="ClubDescription"></textarea>
-                <select name="genre">
-                    <option value="Sports">Sports</option>
-                    <option value="Arts">Arts</option>
-                    <option value="Cycling">Cycling</option>
-                    <option value="Running">Running</option>
-                    <option value="Walking">Walking</option>
-                    <option value="hobby">Hobby</option>
-                    <option value="other">Other</option>
-                </select>
-                <input type="submit">
-            </form>
+        <form action="createclub" method="post">
+            <input type="text" name="clubTitle" placeholder="Club's Name">
+            <textarea name="ClubDescription"></textarea>
+            <select name="genre">
+                <option value="Sports">Sports</option>
+                <option value="Arts">Arts</option>
+                <option value="Cycling">Cycling</option>
+                <option value="Running">Running</option>
+                <option value="Walking">Walking</option>
+                <option value="hobby">Hobby</option>
+                <option value="other">Other</option>
+            </select>
+            <input type="submit">
+        </form>
 
-<?
+        <?
     } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
         include('scripts/dbconnect.php');
         $clubid = str_replace(' ', '-', $_POST["clubTitle"]);
@@ -37,6 +37,8 @@ echo "
             echo "Error: " . $sql . "<br>Error Message:" . mysqli_error($db);
         }
         header("blog");
+    }
+}
 echo "
 </main>
 ";
