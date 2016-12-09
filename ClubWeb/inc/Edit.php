9837db0ebf520e71ae53cea1fd9c0a22c5685f30
1611267 <phaseiimusic@hotmail.com>
2016-12-09 12:34:28 +0000

    Registration page

--- ClubWeb/inc/Edit.php
+++ ClubWeb/inc/Edit.php
@@ -32,8 +32,36 @@ function renderForm($id, $username, $password,$bio,$photoID,$accessLevelID, $db)
 
 include('scripts/dbconnect.php');
 
-if (isset($_POST['submit']))
-{
+$id = $params['userID'];
+
+if (isset($params['userID'])) {
+
+    $result = mysqli_query($db,"SELECT * FROM port_users WHERE userID=$id");
+
+    $row = mysqli_fetch_array($result);
+
+    if($row) {
+
+        $username = $row['username'];
+        $password = $row['password'];
+        $bio = $row['bio'];
+        $photoID = $row['photoID'];
+        $accessLevelID = $row['accessLevelID'];
+        renderForm($id, $username, $password,$bio,$photoID,$accessLevelID,$db);
+
+    } else {
+
+        echo "No results!";
+
+    }
+
+} else {
+
+    echo 'Error!';
+
+}
+
+if (isset($_POST['submit'])) {
 
     if (isset($_POST['id'])) {
 
@@ -67,34 +95,7 @@ if (isset($_POST['submit']))
 
 
 
-    $id = $params['userID'];
 
-    if (isset($params['userID'])) {
-
-        $result = mysqli_query($db,"SELECT * FROM port_users WHERE userID=$id");
-
-        $row = mysqli_fetch_array($result);
-
-        if($row) {
-
-            $username = $row['username'];
-            $password = $row['password'];
-            $bio = $row['bio'];
-            $photoID = $row['photoID'];
-            $accessLevelID = $row['accessLevelID'];
-            renderForm($id, $username, $password,$bio,$photoID,$accessLevelID,$db);
-
-        } else {
-
-            echo "No results!";
-
-        }
-
-    } else {
-
-        echo 'Error!';
-
-    }
 
 }
 
