342d413c12a8406b4c28087850326aaaa0fe864e
1608354 <a>
2016-12-09 18:09:13 +0000

    final css

--- ClubWeb/index.php
+++ ClubWeb/index.php
@@ -24,6 +24,7 @@ $rules = array(
     'Clubs' => "/Clubs/(?'linkref'[\w\-]+)",
     'viewclubs' => "/viewclubs",
     'createclub' => "/createclub",
+    'mapsindex' => "/mapsindex",
     //
     // Home Page
     //
