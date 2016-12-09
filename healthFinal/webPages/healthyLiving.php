b5000a2cc71bf1427d80a8cbfdd55e370a275488
Quinnyy <michaelstvns2@gmail.com>
2016-12-09 19:13:03 +0000

    healthupdates + new footer

--- healthFinal/webPages/healthyLiving.php
+++ healthFinal/webPages/healthyLiving.php
@@ -79,7 +79,7 @@ include '../navBar/master.php';
 <hr class="pacman"/>
 
     <ul class="ulProducts" runat="server" id="newsFeed" style="background-color: #f5f5f5">
-<h2>Article Titles!</h2>
+<h2>Article Titles! </h2>
 
 <?php
 include('dbconnect.php');
@@ -91,7 +91,7 @@ $result = $db->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while ($row = $result->fetch_assoc()) {
-        echo '<li class="display"><b>'.$row["title"].'</li><br>';
+        echo '<li class="display"><b>'.$row["title"].'</b><br>'.$row["text"].'</li>';
 
     }
 }
