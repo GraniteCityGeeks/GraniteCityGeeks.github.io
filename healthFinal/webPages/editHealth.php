905fb52f44d0e301c53b4a64a9353dfc19b9ed70
danny205 <5roundsdanny@gmail.com>
2016-12-10 09:47:05 +0000

    health commit - ud admin edit permitions

--- healthFinal/webPages/editHealth.php
+++ healthFinal/webPages/editHealth.php
@@ -5,7 +5,7 @@ $type = $_POST['type'];
 
 
 if($type=="insert") {
-    $sql = "INSERT INTO port_articles (title, text) VALUES ('" .mysqli_real_escape_string($_POST['title']). "', '" .mysqli_real_escape_string($_POST['desc']). "')";
+    $sql = "INSERT INTO port_articles (title, text) VALUES ('" .mysql_escape_string($_POST['title']). "', '" .mysql_escape_string($_POST['desc']). "')";
     if ($db->query($sql) === TRUE) {
         echo $_POST['title'];
         echo " Article Added, thank you";
@@ -17,7 +17,7 @@ if($type=="insert") {
 if($type=="edit")
 {
      echo $_POST['oldTitle'];
-    $sql = "UPDATE port_articles SET title='".mysqli_real_escape_string($_POST['title'])."',text='".mysqli_real_escape_string($_POST['desc'])."'WHERE title='".mysqli_real_escape_string($_POST['oldTitle'])."'";
+    $sql = "UPDATE port_articles SET title='".mysql_escape_string($_POST['title'])."',text='".mysql_escape_string($_POST['desc'])."'WHERE title='".mysql_escape_string($_POST['oldTitle'])."'";
 
     if ($db->query($sql) === TRUE)
     {
@@ -32,7 +32,7 @@ if($type=="edit")
 if($type=="delete")
 {
     echo $_POST['toDelete'];
-    $sql = "DELETE from port_articles WHERE title='".mysqli_real_escape_string($_POST['toDelete'])."'";
+    $sql = "DELETE from port_articles WHERE title='".mysql_escape_string($_POST['toDelete'])."'";
 
     if ($db->query($sql) === TRUE)
     {
