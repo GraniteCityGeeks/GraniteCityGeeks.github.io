acac686c3e84f1697d2ad7be0c7f00699284fff2
Naelegs <jamiepeterlaw@gmail.com>
2016-12-10 23:37:26 +0000

    fixing murrays bad life choices

--- ClubWeb/inc/404.php
+++ ClubWeb/inc/404.php
@@ -1,2 +1,3 @@
 <h1>404 not found</h1>
-<h2>the requested url was not found on the server</h2>
+<h2>The web page you are looking for cannot be found</h2>
+<h3>Please be patient while the elves from GCG fix this</h3>
