178a1b93db5f24bbab669ff61cfada7772aa1bca
Naelegs <jamiepeterlaw@gmail.com>
2016-12-10 01:10:55 +0000

    css

--- ClubWeb/inc/mapsindex.php
+++ ClubWeb/inc/mapsindex.php
@@ -42,7 +42,7 @@ if(file_exists("/scripts/markers.php")) { echo "markers file found"; } else { ec
             request.send(null);
         }
         //download the locations.
-        downloadUrl("scripts/markers.php", function (data) {
+        downloadUrl("/scripts/markers.php", function (data) {
             var xml = data.responseXML;
             var markers = xml.documentElement.getElementsByTagName("markers");
             for (var i = 0; i < markers.length; i++) {
