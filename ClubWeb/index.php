449629b5629174300ce929d07b26356492579517
Naelegs <jamiepeterlaw@gmail.com>
2016-12-10 18:23:12 +0000

    css

--- ClubWeb/index.php
+++ ClubWeb/index.php
@@ -5,9 +5,6 @@
 
 <body>
 <ul class="nav">
-    <div class="logo">
-        Logo
-    </div>
     <li><a href="page1.html">Page 1</a></li>
     <li><a href="page2.html">Page 2</a></li>
     <li><a href="page3.html">Page 3</a></li>
