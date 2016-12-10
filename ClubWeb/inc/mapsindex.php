adee8f2e62a1dd43f20d2c536da38ed0cce7fc80
Naelegs <jamiepeterlaw@gmail.com>
2016-12-10 13:39:03 +0000

    map

--- ClubWeb/inc/mapsindex.php
+++ ClubWeb/inc/mapsindex.php
@@ -6,7 +6,7 @@ THE NAME OF ANYTHING IN THE JAVASCRIPT AND PLEASE PLEAAAASE DON'T CHANGE THE DIV
 did broke the map's loading functions. -->
 
 <?php
-include '../healthFinal/navBar/master.php';
+
 
 if(file_exists("markers.php")) { echo "markers file found"; } else { echo "markers file missing"; }
 ?>
