b450eb1cdff2ec202c4e96862d81c583e55d0a77
Naelegs <jamiepeterlaw@gmail.com>
2016-12-10 23:33:35 +0000

    fixing murrays bad life choices

--- ClubWeb/index.php
+++ ClubWeb/index.php
@@ -1,6 +1,4 @@
 <?
-
-
 define('INCLUDE_DIR', dirname(__FILE__) . '/inc/');
 $rules = array(
     //
@@ -27,6 +25,7 @@ $rules = array(
     'viewclubs' => "/viewclubs",
     'createclub' => "/createclub",
     'mapsindex' => "/mapsindex",
+    'markers' => "/markers",
     'MarkerAdmin' => "/MarkerAdmin",
     'deletemarker' => "/deletemarker",
     //
@@ -37,19 +36,15 @@ $rules = array(
     // Style
     //
 );
-
-
 $uri = rtrim(dirname($_SERVER["SCRIPT_NAME"]), '/');
 $uri = '/' . trim(str_replace($uri, '', $_SERVER['REQUEST_URI']), '/');
 $uri = urldecode($uri);
 foreach ($rules as $action => $rule) {
-   if (preg_match('~^' . $rule . '$~i', $uri, $params)) {
-       include(INCLUDE_DIR . $action . '.php');
-       exit();
-   }
+    if (preg_match('~^' . $rule . '$~i', $uri, $params)) {
+        include(INCLUDE_DIR . $action . '.php');
+        exit();
+    }
 }
 // nothing is found so handle the 404 error
 include(INCLUDE_DIR . '404.php');
 ?>
-
-
