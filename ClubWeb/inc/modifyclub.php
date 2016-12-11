dc5436417d5fcc04dcdc2a3befebb9cf22356ba6
Medscootsman <murraylyne2@Gmail.com>
2016-12-11 00:31:11 +0000

    whoops! fetchquery is not a function

--- ClubWeb/inc/modifyclub.php
+++ ClubWeb/inc/modifyclub.php
@@ -53,7 +53,7 @@ if (isset ($_SESSION['username'])) {
         $clubcalendar = $_POST['clubcalendar'];
         $insert = ("UPDATE port_club SET description = '$clubdesc', clubcalendar = '$clubcalendar' WHERE clubid = $id");
         if (mysqli_query($db, $insert)) {
-            header('Location: Clubs');
+            header('Location: viewclubs');
         }  else {
         echo "Error: " . $insert . "<br>Error Message:" . mysqli_error($db);
     }
