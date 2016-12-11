97f68a012ca24fc7a2d9377b97747370d161b940
Medscootsman <murraylyne2@Gmail.com>
2016-12-11 00:05:52 +0000

    whoops! fetchquery is not a function

--- ClubWeb/inc/modifyclub.php
+++ ClubWeb/inc/modifyclub.php
@@ -19,12 +19,37 @@ if (isset ($_SESSION['username'])) {
         if (!$result) {
             header('Location: Clubs');
 
-        } else {
+        } else  {
             //if a club has been found, continue.
+            //get results from the query.
+
+            while($row = $result->fetch_array()) {
+                $clubid = $row['clubid'];
+                $clubname = $row['clubTitle'];
+                $clubdesc = $row['description'];
+                $clubcalendar = $row['clubcalendar'];
+
+
+            }
+            echo "<h1> Currently editing: $clubname </h1>";
+            echo "<form name ='clubsubmit' method='POST'>";
+            echo "<input type='text' name='clubdesc'> value='$clubdesc'";
+            echo "<input type='text' name='clubcalendar'> value='$clubcalendar'";
+            echo "<input type='submit' value='submit'>";
+            echo "<h4>Users in club</h4>";
+            echo "</form>";
+
 
 
         }
 
-        //echo "<input type='text' name='clubdesc'> value=''";
+
+    } else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $cludesc = $_POST['clubdesc'];
+        $clubcalendar = $_POST['clubcalendar'];
+        $insert = ("UPDATE port_club SET description = '$clubdesc', clubcalendar = '$clubcalendar' WHERE clubid = $clubid");
+        if (mysqli_query($db, $insert)) {
+            echo "<p> edit successful! </p>";
+        }
     }
 }
