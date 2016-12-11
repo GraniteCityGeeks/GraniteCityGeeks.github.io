f573ff1ca20c90a130abf03496fdfffd8dbec912
Medscootsman <murraylyne2@Gmail.com>
2016-12-11 23:36:12 +0000

    whoops! fetchquery is not a function

--- ClubWeb/inc/Clubs.php
+++ ClubWeb/inc/Clubs.php
@@ -63,7 +63,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     }
     echo "</table>";
 
-    echo "<form action='Clubs' method = 'POST'>";
+    echo "<form action='Clubs/{$club}' method = 'POST'>";
     echo "<button type='submit' name='clubid' height='100' width='150' value='$club'>Join this club!</button>";
     echo "</form>";
 
@@ -128,7 +128,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         $insertquery = ("INSERT INTO port_usersinclub(userid, clubid) VALUES($id, $clubid)");
 
         if (mysqli_query($db, $insertquery)) {
-            header("Location: Clubs");
+            header("Location: viewclubs");
         }
     } else {
         header('Location: login');
