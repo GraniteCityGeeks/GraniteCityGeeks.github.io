f45727dacf185fe23e837c9303ae67f0889ec5e8
Jordan <jwyoungman96@hotmail.co.uk>
2016-12-11 23:01:32 +0000

    test

--- ClubWeb/inc/scripts/header_l2.php
+++ ClubWeb/inc/scripts/header_l2.php
@@ -1,32 +1,44 @@
 <?
 session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+<link rel="stylesheet" href="/basicstyle.css">
 <head>
+
     <meta charset="UTF-8">
-    <title>The Blog of Charles Babbage</title>
-    <link rel="stylesheet" href="/CoreCSS/NavbarMaster.css">
+    <title>Sportlethen</title>
 
 </head>
 
-<body>
-<header id="myCarousel" class="carousel slide">
-    <h1>The Blog of Charles Babbage</h1>
+<body class="master-body">
+<header class="header">
+    <h1>Sportlethen</h1>
+    <?
+    if (isset($_SESSION['username'])) {
+        echo "<h3><a href='logout'>Logout({$_SESSION['username']})<img src={$_SESSION['photoID']} alt=\"Mountain View\" style=\"width:35px;height:35px;\"></a></h3>";
+    } else {
+        echo "<h3><a href=' login '>Login</a></h3>";
+        echo "<h3><a href=' register '>Register</a></h3>";
+    }
+    ?>
     <nav>
-        <ul>
+        <ul id="nav">
             <li><a href="../">Home Page</a></li>
             <li><a href="../blog">My Blog</a></li>
-            <li><a href="../about">About Me</a></li>
+            <li><a href=../"about">About Me</a></li>
             <li><a href="../contactus">Contact Me</a></li>
+            <li><a href="../viewclubs">Clubs</a></li>
+            <li><a href="../mapsindex">Maps</a></li>
             <?
-            if (isset($_SESSION['username'])) {
-                echo "<li><a href='../createarticle'>Create Article</a></li>";
-                echo "<li><a href='../createclub'>Create Club </a></li>";
-                echo "<li><a href='../logout'>Logout</a></li>";
-            } else {
-                echo "<li><a href='../login'>Login</a></li>";
+            if (isset($_SESSION['accessLevelID']) == 2){
+                echo "<li><a href='../createarticle'>Create Art</a></li>";
+                echo "<li><a href='../View'>View</a></li>";
+                echo "<li><a href='../createclub'>Create Clubs</a></li>";
+                echo "<li><a href='../modifyclub'>modify your Club</a></li>";
             }
             ?>
 
