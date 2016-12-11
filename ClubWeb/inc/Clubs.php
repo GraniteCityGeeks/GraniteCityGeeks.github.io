bb905f53280aa1469c0d2ef781f1b12b5252660e
Medscootsman <murraylyne2@Gmail.com>
2016-12-11 23:24:53 +0000

    whoops! fetchquery is not a function

--- ClubWeb/inc/Clubs.php
+++ ClubWeb/inc/Clubs.php
@@ -95,36 +95,43 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 } ELSE IF($_SERVER['REQUEST_METHOD'] === 'POST') {
     include('dbconnect.php');
     session_start();
-    $username = $_SESSION['username'];
-    $clubid = $_POST['clubid'];
+
+    //before we continue, check that there IS a username
+    if (isset($_SESSION['username'])) {
+        $username = $_SESSION['username'];
+
+        $clubid = $_POST['clubid'];
 
 //find the username's id via query
 
-    $queryuserid = ("SELECT userID from port_users where username = '$username'");
+        $queryuserid = ("SELECT userID from port_users where username = '$username'");
 
-    $resultuserid = $db->query($queryuserid);
+        $resultuserid = $db->query($queryuserid);
 
-    while($row = $resultuserid->fetch_array()) {
-        $id = $row['userID'];
-    }
+        while ($row = $resultuserid->fetch_array()) {
+            $id = $row['userID'];
+        }
 
 //before inserting check if id is already in club.
 
-    $checkquery = ("SELECT userid, clubid from port_usersinclubs WHERE userid = '$id' AND clubid = ''$clubid'");
+        $checkquery = ("SELECT userid, clubid from port_usersinclubs WHERE userid = '$id' AND clubid = ''$clubid'");
 
-    $checkresult = $db->query($checkquery);
+        $checkresult = $db->query($checkquery);
 
-    if ($checkresult->num_rows>0) {
-        echo '<script language="javascript">';
-        echo 'alert("user is already in club!")';
-        echo '</script>';
-        header("Location: viewclubs");
+        if ($checkresult->num_rows > 0) {
+            echo '<script language="javascript">';
+            echo 'alert("user is already in club!")';
+            echo '</script>';
+            header("Location: viewclubs");
 
-    }
-    $insertquery = ("INSERT INTO port_usersinclub(userid, clubid) VALUES($id, $clubid)");
+        }
+        $insertquery = ("INSERT INTO port_usersinclub(userid, clubid) VALUES($id, $clubid)");
 
-    if (mysqli_query($db, $insertquery)) {
-        header("Location: Clubs");
+        if (mysqli_query($db, $insertquery)) {
+            header("Location: Clubs");
+        }
+    } else {
+        header('Location: login');
     }
 }
     ?>
