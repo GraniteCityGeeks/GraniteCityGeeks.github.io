db852586be25626a908c05f7b43428fbe6db46e0
Medscootsman <murraylyne2@Gmail.com>
2016-12-11 17:16:29 +0000

    whoops! fetchquery is not a function

--- ClubWeb/inc/create_club_article.php
+++ ClubWeb/inc/create_club_article.php
@@ -10,7 +10,7 @@ if (isset($_SESSION['username'])) //SESSION DOES EXIST
             <script>tinymce.init({selector: 'textarea'});</script>
             <form action="createarticle" method="POST">
                 <input type="text" name="clubarticlename\" placeholder="Article Name">
-                <textarea name=\"articleText\"></textarea>
+                <textarea name="articleText"></textarea>
                 <label for="club">select club to make article for.</label>
                 <select name="club">
                     <?
