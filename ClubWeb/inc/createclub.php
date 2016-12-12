1c19541de536e1e2660fe4becc3d2c78ea446bdc
John Doe <murraylyne2@Gmail.com>
2016-12-12 20:26:28 +0000

    viewclubs

--- ClubWeb/inc/createclub.php
+++ ClubWeb/inc/createclub.php
@@ -77,8 +77,8 @@ if (isset($_SESSION['username'])) //SESSION DOES EXIST
         $clubtxt = $_POST["ClubDescription"];
         $clubgenre = $_POST["genre"];
         $clubavatar = $_POST["avatar"];
-        $db->real_escape_string($clubtitle);
-        $db->real_escape_string($clubtxt);
+        $clubtitle = $db->real_escape_string($clubtitle);
+        $clubtxt = $db->real_escape_string($clubtxt);
         $sql = "INSERT INTO port_club(clubTitle, description, genreid, photoid, clubcalendar, ownerid) VALUES('$clubtitle', '$clubtxt', '$clubgenre', '$clubavatar', 'No events upcoming', $userid)";
 
 
