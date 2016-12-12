7f1273cab99a99c076ca65f35c6c0e9887bca8e4
danny205 <5roundsdanny@gmail.com>
2016-12-12 14:06:23 +0000

    health commit - ud admin edit permitions

--- ClubWeb/inc/healthFinal/webPages/healthyLiving.php
+++ ClubWeb/inc/healthFinal/webPages/healthyLiving.php
@@ -49,58 +49,58 @@ include '../navBar/master.php';
 
 
 
-<ul class="ulProducts" runat="server" id="newsFeed" style="background-color: #f5f5f5">
-    <h2>News Feed!</h2>
-    <?php
-    include('dbconnect.php');
-    /* this script loads the article the user clicked on.*/
+    <ul class="ulProducts" runat="server" id="newsFeed" style="background-color: #f5f5f5">
+        <h2>News Feed!</h2>
+        <?php
+        include('dbconnect.php');
+        /* this script loads the article the user clicked on.*/
 
-    $sql = "SELECT * FROM port_newsfeed";
-    $result = $db->query($sql);
+        $sql = "SELECT * FROM port_newsfeed";
+        $result = $db->query($sql);
 
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while ($row = $result->fetch_assoc()) {
-            echo '<li class="display2"><b>'.$row["title"].'</b><br>'.$row["text"].'</li>';
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while ($row = $result->fetch_assoc()) {
+                echo '<li class="display2"><b>'.$row["title"].'</b><br>'.$row["text"].'</li>';
+            }
         }
-    }
-    else {
-        echo "0 results";
-    }
-    $db->close();
-    ?>
+        else {
+            echo "0 results";
+        }
+        $db->close();
+        ?>
     </ul>
 </div>
 <br>
 
 <div class="container">
-<h2>Community Spotlight!</h2>
-<i>Articles published by users of this site!</i>
-<hr class="pacman"/>
+    <h2>Community Spotlight!</h2>
+    <i>Articles published by users of this site!</i>
+    <hr class="pacman"/>
 
     <ul class="ulProducts" runat="server" id="newsFeed" style="background-color: #f5f5f5">
-<h2>Article Titles! </h2>
+        <h2>Article Titles! </h2>
 
-<?php
-include('dbconnect.php');
-/* this script loads the article the user clicked on.*/
+        <?php
+        include('dbconnect.php');
+        /* this script loads the article the user clicked on.*/
 
-$sql = "SELECT * FROM port_articles";
-$result = $db->query($sql);
+        $sql = "SELECT * FROM port_articles";
+        $result = $db->query($sql);
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while ($row = $result->fetch_assoc()) {
-        echo '<li class="display"><b>'.$row["title"].'</b><br>'.$row["text"].'</li><br>';
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while ($row = $result->fetch_assoc()) {
+                echo '<li class="display"><b>'.$row["title"].'</b><br>'.$row["text"].'</li><br>';
 
-    }
-}
-else {
-    echo "0 results";
-    }
-$db->close();
-?>
-</ul>
+            }
+        }
+        else {
+            echo "0 results";
+        }
+        $db->close();
+        ?>
+    </ul>
 
 
 
@@ -112,9 +112,9 @@ $db->close();
             <div id="twitter" style="float:left;">
                 <a class="twitter-timeline"  data-width="1550" data-height="700" d href="https://twitter.com/bbchealth">Tweets by bbchealth</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
             </div>
-            </div>
         </div>
     </div>
+</div>
 
 
 <?php
