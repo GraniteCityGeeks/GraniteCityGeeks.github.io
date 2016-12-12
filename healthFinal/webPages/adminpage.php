fb247e105dc00c8880936112ad4a4555cd765a9e
danny205 <5roundsdanny@gmail.com>
2016-12-12 13:38:02 +0000

    health commit - ud admin edit permitions

--- healthFinal/webPages/adminpage.php
+++ healthFinal/webPages/adminpage.php
@@ -1,12 +1,13 @@
 <?php
 include '../navBar/master.php';
 ?>
-<?
+<?php
 session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-f (isset($_SESSION['accessLevelID']) == 5){echo "";}else{
+if(isset($_SESSION['accessLevelID']) == 5){echo "";}else{
+
     header("Location: http://gcg.azurewebsites.net/healthFinal/webPages/healthyliving.php");
 
 }
