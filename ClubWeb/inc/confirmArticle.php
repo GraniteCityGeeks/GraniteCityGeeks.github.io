98fcff1bb24ee794d87c352e686a2d9466cf9d6f
1611267 <phaseiimusic@hotmail.com>
2016-12-12 15:16:54 +0000

    Registration page

--- ClubWeb/inc/confirmArticle.php
+++ ClubWeb/inc/confirmArticle.php
@@ -3,6 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 $clubarticleid = $params['clubarticleid'];
+
 include("scripts/dbconnect.php");
 
 if (isset($params['clubarticleid']))
@@ -12,14 +13,14 @@ if (isset($params['clubarticleid']))
     $sql = "UPDATE port_club_article SET confirmed = '1' WHERE clubarticleid='$clubarticleid'";
     $result = $db->query($sql);
 
-    header("Location: /ClubWeb/ViewArticles");
+    //header("Location: /ClubWeb/ViewArticles");
 
 }
 
 else
 
 {
-    header("Location: /ClubWeb/ViewArticles");
+    //header("Location: /ClubWeb/ViewArticles");
 }
 
 
