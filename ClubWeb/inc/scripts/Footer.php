ca4c615394f700f715aef92cde4cadfa0c7e0295
JordanYoungman <jwyoungman96@homail.co.uk>
2016-12-12 16:14:16 +0000

    hi

--- ClubWeb/inc/scripts/Footer.php
+++ ClubWeb/inc/scripts/Footer.php
@@ -1,3 +1,11 @@
-<footer>
-    <b>Copyright Granite City Geeks © 2016 All Rights Reserved</b>
+<link href="http://gcg.azurewebsites.net/healthFinal/CSS/footer-basic-centered.css?version=51" rel="stylesheet" />
+
+<footer class="footer-basic-centered">
+
+    <p class="footer-company-motto">Discover Aberdeen</p>
+
+    <p class="footer-company-name">GraniteCityGeeks &copy; 2016</p>
+
 </footer>
+
+</html>
