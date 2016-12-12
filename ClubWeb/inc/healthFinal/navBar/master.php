7bf319735e1a3c1f0285d7fdf1b6bae12c09d5b5
danny205 <5roundsdanny@gmail.com>
2016-12-12 14:05:33 +0000

    health commit - ud admin edit permitions

---
+++ ClubWeb/inc/healthFinal/navBar/master.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head runat = "server">
+    <title>
+        Discover Aberdeen
+    </title>
+
+
+    <link href="/healthFinal/CSS/NavbarMaster.css" rel="stylesheet" />
+
+</head >
+
+<body class="master-body">
+<form id="form1" runat="server" style="height: 50px">
+    <header >
+        <nav>
+            <ul>
+                <li><a href="/healthFinal/webPages/index.php"><b>Home</b></a></li>
+                <li>
+                    <a href="/ClubWeb/inc/Home.php"><b>Clubs</b></a>
+                </li>
+                <li><a href="/healthFinal/webPages/healthyLiving.php"><b>Healthy Living</b></a></li>
+                <li><a href="/ClubWeb/inc/mapsindex.php"><b>Local Trails</b></a></li>
+                <li><a href="#"><b>Login</b></a></li>
+                <li><a href="#"><b>Site Users</b></a>
+                    <ul>
+                        <li><a href="#">Admin</a>
+                        <ul>
+                            <li><a href="#">Clubs</a></li>
+                            <li><a href="/healthFinal/webPages/adminpage.php">Health</a></li>
+                            <li><a href="#">Maps</a></li>
+                        </ul></li>
+                        <li><a href="#">Contributor</a>
+                         <ul>
+                            <li><a href="#">Clubs</a></li>
+                            <li><a href="/healthFinal/webPages/contributorPage.php">Health</a></li>
+                            <li><a href="#">Maps</a></li>
+
+                        </ul>
+                     </ul>
+                         </li>
+
+            </ul>
+
+                <ul class="right">
+                     <li ><label style="Width=300px Font-Bold=true" ID="WelcomeLabel">Welcome User</label> </li>
+                </ul>
+
+        </nav>
+
+    </header>
+</form>
+</body>
+</html>
+
+<?php
+?>
