c3038071c68344f75d28b7310a7c9cdbf701c4b9
Jamie Law <jamiepeterlaw@gmail.com>
2017-08-27 19:41:14 +0100

    css rework

--- index.php
+++ index.php
@@ -9,7 +9,25 @@
 <link href="../CoreCSS/healthPage.css" rel="stylesheet" />
 -->
 <head>
-    <title> Go Portlethen </title>
+    <!-- Title -->
+    <title>Go Portlethen!</title>
+    <!-- Meta -->
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="Portlethen (and surrounding communities) Information Resource">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <!-- Favicon -->
+    <link href="favicon.ico" rel="shortcut icon">
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="Style/assets/css/bootstrap.css" rel="stylesheet">
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="Style/assets/css/animate.css" rel="stylesheet">
+    <link rel="stylesheet" href="Style/assets/css/font-awesome.css" rel="stylesheet">
+    <link rel="stylesheet" href="Style/assets/css/nexus.css" rel="stylesheet">
+    <link rel="stylesheet" href="Style/assets/css/responsive.css" rel="stylesheet">
+    <link rel="stylesheet" href="Style/assets/css/custom.css" rel="stylesheet">
+    <!-- Google Fonts-->
+    <link href="http://fonts.googleapis.com/css?family=Roboto+Condensed:400,300" rel="stylesheet" type="text/css">
 </head>
 <h1> Granite City Geeks - Go Portlethen</h1>
 <h2> Discover Aberdeen </h2>
