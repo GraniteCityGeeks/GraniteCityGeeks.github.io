0d054ecee3790ca74ea5f5169652d316ccea25f1
Jamie Law <jamiepeterlaw@gmail.com>
2017-08-29 13:19:17 +0100

    mapsnew

--- ClubWeb/inc/mapsindex.php
+++ ClubWeb/inc/mapsindex.php
@@ -1,123 +1,184 @@
-<!-- Michael, i brought back an older version because the map kept breaking. This map doesn't break. PLEASE DO NOT CHANGE
-THE NAME OF ANYTHING IN THE JAVASCRIPT AND PLEASE PLEAAAASE DON'T CHANGE THE DIV TAGS WITHOUT MY PERMISSION. Whatever you/jamie
-did broke the map's loading functions. -->
-
-<?php
-include("scripts/dbconnect.php");
-include("scripts/Header.php")
-?>
-
 <html>
+
 <head>
-    <link href="/basicstyle.css" rel="stylesheet" type="text/css">
-
-    <title>GoPortlethen - Maps</title>
-    <style>
-        @import url('https://fonts.googleapis.com/css?family=Roboto');
-    </style>
 
 
-    <script type="text/javascript">
-        function initMap() {
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: {lat: 57.061681, lng: -2.129468},
-                zoom: 12,
-                MapTypeId: "satellite"
-            });
-        }
-    </script>
 
-    <script type="text/javascript">
-        //Get the markers from the XML document.
-        function downloadUrl(url, callback) {
-            var request = window.ActiveXObject ?
-                new ActiveXObject('Microsoft.XMLHTTP') :
-                new XMLHttpRequest;
-            request.onreadystatechange = function () {
-                if (request.readyState == 4) {
-                    callback(request, request.status);
-                }
-            };
-            request.open('GET', url, true);
-            request.send(null);
-        }
-        //download the locations.
-        downloadUrl("markers", function (data) {
-            var xml = data.responseXML;
-            var markers = xml.documentElement.getElementsByTagName('markers');
-            for (var i = 0; i < markers.length; i++) {
-                var desc = markers[i].getAttribute("description");
-                var name = markers[i].getAttribute("name");
-                var point = new google.maps.LatLng(
-                    parseFloat(markers[i].getAttribute("lat")),
-                    parseFloat(markers[i].getAttribute("lng"))
-                );
-                addMarker(i, point, desc, name);
-            }
-        });
-        var markersArray = [];  //array for markers
-        //this fucntion will add the markers.
-        function addMarker(id,point,description,name) {
-            id = new google.maps.Marker({
-                map: map,
-                draggable: false,
-                animation: google.maps.Animation.DROP,
-                position: point
-            });
-            var contentstring = '<div id="content">' +
-                '<h1>'+ name +'</h1>'+
-                '<p>'+ description +'</p>';
-            var infowindow = new google.maps.InfoWindow({
-                content:contentstring
-            });
-            id.addListener('click', function() {
-                map.setZoom(15);
-                map.setCenter(id.getPosition());
-                infowindow.open(map, id);
-            });
-            markersArray.push(id);
-        }
-    </script>
-    <script async defer
-            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCC8HwZx1Aknt-BHgT2vYtcgeBBvokVzWU&callback=initMap">
-    </script>
+    <?php
+    include("dbconnect.php");
+    ?>
+  <!-- Title -->
+  <title>Go Portlethen!</title>
+  <!-- Meta -->
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <meta name="description" content="Portlethen (and surrounding communities) Information Resource">
+  <meta name="author" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 
-    <meta charset="UTF-8">
+
+  <link href="http://fonts.googleapis.com/css?family=Roboto+Condensed:400,300" rel="stylesheet" type="text/css">
 </head>
 
 <body>
-<div id="container">
-    <div id="main">
+
+
+
+<!-- sidebar -->
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-2 col-sm-4">
+        <div class="heading">
+          <h4><strong> Portlethen map </strong> </h4>
+        </div>
         <br>
-        <br>
+        <div class="left-navigation">
+          <ul class="control-list" id="searchControls">
+            <label for="txtboxSearch">Look up routes </label>
+            <li> <input type="textbox" class="form-control" placeholder="search here" id="txtboxSearch"> </li>
+            <li> <label class="radio-inline"> <input type="radio" name="radioRouteFilter"> Cycling routes  </label>  </li>
+            <li> <label class="radio-inline"> <input type="radio" name="radioRouteFilter"> Running routes </label> </li>
+            <li> <label class="radio-inline"> <input type="radio" name="radioRouteFilter"> Walking routes </label> </li>
+            <li> <label class="radio-inline"> <input type="radio" name="radioRouteFilter"> N/A </label> </li>
+            <br>
+            <h5> Route Location </h5>
+            <li>
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox" value="" id="chkboxUrbanOnly"> Urban only
+                </label>
+              </div>
+            </li>
+            <li>
+            <div class="checkbox">
+                <label>
+                  <input type="checkbox" value="" id="chkboxRuralOnly"> Rural only
+                </label>
+            </div>
+            </li>
+            <div class="checkbox">
+                <label>
+                  <input type="checkbox" value="" id="chkboxHybrid"> Hybrid
+                </label>
+            </div>
+            </li>
+          </ul>
 
+          <br>
 
+          <ul class="control-list" id="displayOptions">
+            <h5><strong>display options</strong></h5>
+            <li>
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox" value="">Markers
+                </label>
+              </div>
+            </li>
+            <li>
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox" value="">Routes
+                </label>
+              </div>
+            </li>
 
-        <div id="map" style="height:400px;width:60%; float:left;">
-            <h2>Map ^^^^^^</h2>
+            <br>
+
+            <li>
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox" value="">Restrict range
+                </label>
+              </div>
+            </li>
+
+            <li>
+              <label for="rangeRestrict">Range of search (1-100 Miles from specified marker)</label>
+              <input type="range" min="0" max="100" id="rangeRestrict">
+            </li>
+
+          </ul>
+        </div>
+      </div>
+      <div class="col-lg-2 col-sm-3">
+        <h5>Route Info </h5>
+        <div class="form-inline">
+          <label>Total Miles: </label>
+          <label id="lblDistance"></label>
         </div>
 
-        <div id="sidebar">
-            <a class="twitter-timeline" data-width="400" data-height="800" d href="https://twitter.com/PortlethenGC">Tweets by PortlethenGC</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+        <div class="form-inline">
+          <label>Average Time: </label>
+          <label id="lblTime"></label>
         </div>
 
+
+      </div>
+      <!-- map -->
+      <div class="col-lg-8 col-sm-7">
+        <div id="map"></div>
+      </div>
     </div>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+  </div>
+
+  <!-- JS -->
+  <script type="text/javascript" src="HTML/assets/js/jquery.min.js" type="text/javascript"></script>
+  <script type="text/javascript" src="HTML/assets/js/bootstrap.min.js" type="text/javascript"></script>
+  <script type="text/javascript" src="HTML/assets/js/scripts.js"></script>
+  <!-- Isotope - Portfolio Sorting -->
+  <script type="text/javascript" src="HTML/assets/js/jquery.isotope.js" type="text/javascript"></script>
+  <!-- Mobile Menu - Slicknav -->
+  <script type="text/javascript" src="HTML/assets/js/jquery.slicknav.js" type="text/javascript"></script>
+  <!-- Animate on Scroll-->
+  <script type="text/javascript" src="HTML/assets/js/jquery.visible.js" charset="utf-8"></script>
+  <!-- Sticky Div -->
+  <script type="text/javascript" src="HTML/assets/js/jquery.sticky.js" charset="utf-8"></script>
+  <!-- Slimbox2-->
+  <script type="text/javascript" src="HTML/assets/js/slimbox2.js" charset="utf-8"></script>
+  <!-- Modernizr -->
+  <script src="HTML/assets/js/modernizr.custom.js" type="text/javascript"></script>
+  <!-- More maps -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCC8HwZx1Aknt-BHgT2vYtcgeBBvokVzWU&callback=initMap"
+    async defer></script>
+  <script type="text/javascript" src="HTML/assets/js/map.js" type="text/javascript"></script>
+  <!-- End JS -->
+  <!-- Footer -->
+  <div id="footer" class="background-grey">
+      <div class="container">
+          <div class="row">
+              <!-- Footer Menu -->
+              <div id="footermenu" class="col-md-8">
 
 
 
-    <br>
+              <!-- End Footer Menu -->
+              <!-- Copyright -->
+              <div id="copyright" class="col-md-4">
+                  <p class="pull-right">(c) 2017 Vanilla Latte</p>
+              </div>
+              <!-- End Copyright -->
+          </div>
+      </div>
+  </div>
+  <!-- End Footer -->
 
 
-</div>
-<footer>
-    <B>Copyright Granite City Geeks © 2016 All Rights Reserved</B>
-</footer>
-</div>
+  <!-- JS -->
+  <script type="text/javascript" src="HTML/assets/js/jquery.min.js" type="text/javascript"></script>
+  <script type="text/javascript" src="HTML/assets/js/bootstrap.min.js" type="text/javascript"></script>
+  <script type="text/javascript" src="HTML/assets/js/scripts.js"></script>
+  <!-- Isotope - Portfolio Sorting -->
+  <script type="text/javascript" src="HTML/assets/js/jquery.isotope.js" type="text/javascript"></script>
+  <!-- Mobile Menu - Slicknav -->
+  <script type="text/javascript" src="HTML/assets/js/jquery.slicknav.js" type="text/javascript"></script>
+  <!-- Animate on Scroll-->
+  <script type="text/javascript" src=HTML/"assets/js/jquery.visible.js" charset="utf-8"></script>
+  <!-- Sticky Div -->
+  <script type="text/javascript" src="HTML/assets/js/jquery.sticky.js" charset="utf-8"></script>
+  <!-- Slimbox2-->
+  <script type="text/javascript" src="HTML/assets/js/slimbox2.js" charset="utf-8"></script>
+  <!-- Modernizr -->
+  <script src="HTML/assets/js/modernizr.custom.js" type="text/javascript"></script>
+  <!-- End JS -->
 </body>
-
 </html>
