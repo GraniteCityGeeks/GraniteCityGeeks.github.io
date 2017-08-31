a5844d35082ecf74f62b1460b703079b2dd06976
Medscootsman <murraylyne2@Gmail.com>
2017-08-31 21:08:36 +0100

    PLS

--- ClubWeb/inc/mapsindex.php
+++ ClubWeb/inc/mapsindex.php
@@ -122,19 +122,19 @@
   </div>
 
   <!-- JS -->
-  <script type="text/javascript" src="HTML/assets/js/jquery.min.js" type="text/javascript"></script>
-  <script type="text/javascript" src="HTML/assets/js/bootstrap.min.js" type="text/javascript"></script>
-  <script type="text/javascript" src="HTML/assets/js/scripts.js"></script>
+  <script type="text/javascript" src="assets/js/jquery.min.js" type="text/javascript"></script>
+  <script type="text/javascript" src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+  <script type="text/javascript" src="assets/js/scripts.js"></script>
   <!-- Isotope - Portfolio Sorting -->
-  <script type="text/javascript" src="HTML/assets/js/jquery.isotope.js" type="text/javascript"></script>
+  <script type="text/javascript" src="assets/js/jquery.isotope.js" type="text/javascript"></script>
   <!-- Mobile Menu - Slicknav -->
-  <script type="text/javascript" src="HTML/assets/js/jquery.slicknav.js" type="text/javascript"></script>
+  <script type="text/javascript" src="assets/js/jquery.slicknav.js" type="text/javascript"></script>
   <!-- Animate on Scroll-->
-  <script type="text/javascript" src="HTML/assets/js/jquery.visible.js" charset="utf-8"></script>
+  <script type="text/javascript" src="assets/js/jquery.visible.js" charset="utf-8"></script>
   <!-- Sticky Div -->
-  <script type="text/javascript" src="HTML/assets/js/jquery.sticky.js" charset="utf-8"></script>
+  <script type="text/javascript" src="assets/js/jquery.sticky.js" charset="utf-8"></script>
   <!-- Slimbox2-->
-  <script type="text/javascript" src="HTML/assets/js/slimbox2.js" charset="utf-8"></script>
+  <script type="text/javascript" src="assets/js/slimbox2.js" charset="utf-8"></script>
   <!-- Modernizr -->
   <script src="HTML/assets/js/modernizr.custom.js" type="text/javascript"></script>
   <!-- More maps -->
