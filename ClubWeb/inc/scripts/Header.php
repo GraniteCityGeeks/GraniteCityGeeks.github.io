45a104469f95741eb047d887660bf3a0b6356ce0
Jamie Law <jamiepeterlaw@gmail.com>
2017-08-31 22:43:08 +0100

    path

--- ClubWeb/inc/scripts/Header.php
+++ ClubWeb/inc/scripts/Header.php
@@ -8,7 +8,7 @@ session_start();
     <!-- Title -->
     <title>Go Portlethen!</title>
     <!-- Favicon -->
-    <link rel="shortcut icon" href="ClubWeb/inc/scripts/Style/favicon.ico" />
+    <link rel="icon" href="inc/scripts/Style/favicon.ico" />
     <!-- Meta -->
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <meta name="description" content="Portlethen (and surrounding communities) Information Resource">
