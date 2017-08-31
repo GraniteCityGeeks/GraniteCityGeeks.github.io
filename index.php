aea1bc2e737e1e6eee28418698ff8fd87a1adc5e
Jamie Law <jamiepeterlaw@gmail.com>
2017-08-31 16:35:53 +0100

    reorganise

--- index.php
+++ index.php
@@ -16,35 +16,6 @@
 
 
 <body>
-
-
-
-</ul>
-
-<div id="main">
-
-
-
-
-    <div id="column1">
-        Portlethen (/pɔərtˈlɛθən/; Scottish Gaelic: Port Leathain) is a town located approximately 7 miles south of Aberdeen, Scotland along the A90. The population according to the 2011 census was 7,130 [2] making it the seventh most populous settlement within Aberdeenshire.
-        <br>
-        </hr>
-        Please Click "Clubs" to login/register to the site.
-
-
-    </div>
-    <div id="sidebar">
-
-        Portlethen lies about two kilometres east of the ancient Causey Mounth road, which was built on high ground to make passable this only available medieval route across the Mounth from coastal points south to Aberdeen. This ancient passage specifically connected the Bridge of Dee with Muchalls Castle and Stonehaven to the south.[5] The route was that taken by the William Keith, 7th Earl Marischal and James Graham, 1st Marquess of Montrose when they led a Covenanter army of 9000 men in the first battle of the Civil War in 1639.[6] Portlethen has expanded very rapidly. In the 1980s a new retail park was constructed. Portlethen is still continuing to expand into a sizable town.
-    </div>
-
-
-</div>
-
-
-</div>
-
 <?PHP
 include ("ClubWeb/inc/scripts/Footer.php");
 ?>
@@ -137,6 +108,25 @@ include ("ClubWeb/inc/scripts/Footer.php");
         <!-- === END HEADER === -->
 </header>
 
+
+
+<div id="main">
+
+
+
+
+    <div id="column1">
+        Portlethen (/pɔərtˈlɛθən/; Scottish Gaelic: Port Leathain) is a town located approximately 7 miles south of Aberdeen, Scotland along the A90. The population according to the 2011 census was 7,130 [2] making it the seventh most populous settlement within Aberdeenshire.
+        <br>
+        </hr>
+        Please Click "Clubs" to login/register to the site.
+
+
+    </div>
+    <div id="sidebar">
+
+        Portlethen lies about two kilometres east of the ancient Causey Mounth road, which was built on high ground to make passable this only available medieval route across the Mounth from coastal points south to Aberdeen. This ancient passage specifically connected the Bridge of Dee with Muchalls Castle and Stonehaven to the south.[5] The route was that taken by the William Keith, 7th Earl Marischal and James Graham, 1st Marquess of Montrose when they led a Covenanter army of 9000 men in the first battle of the Civil War in 1639.[6] Portlethen has expanded very rapidly. In the 1980s a new retail park was constructed. Portlethen is still continuing to expand into a sizable town.
+    </div>
 </html>
 <?php
 include("ClubWeb/inc/scripts/dbconnect.php");
