f05f3cd43d317846a34e7116800d479ddb8b5f3f
Jumbo the code warlock <jamiepeterlaw@gmail.com>
2017-09-07 22:02:17 +0100

    fix

--- index.php
+++ index.php
@@ -237,15 +237,15 @@ include("ClubWeb/inc/scripts/header.php");
           </div>
           <!-- End Footer -->
           <!-- JS -->
-          <script type="text/javascript" src="ClubWeb/inc/Style/assets/js/jquery.min.js" type="text/javascript"></script>
-          <script type="text/javascript" src="ClubWeb/inc/Style/assets/js/bootstrap.min.js" type="text/javascript"></script>
-          <script type="text/javascript" src="ClubWeb/inc/Style/assets/js/scripts.js"></script>
+          <script type="text/javascript" src="/ClubWeb/inc/Style/assets/js/jquery.min.js" type="text/javascript"></script>
+          <script type="text/javascript" src="/ClubWeb/inc/Style/assets/js/bootstrap.min.js" type="text/javascript"></script>
+          <script type="text/javascript" src="/ClubWeb/inc/Style/assets/js/scripts.js"></script>
           <!-- Isotope - Portfolio Sorting -->
           <script type="text/javascript" src="ClubWeb/inc/Style/assets/js/jquery.isotope.js" type="text/javascript"></script>
           <!-- Mobile Menu - Slicknav -->
           <script type="text/javascript" src="ClubWeb/inc/Style/assets/js/jquery.slicknav.js" type="text/javascript"></script>
           <!-- Animate on Scroll-->
-          <script type="text/javascript" src=ClubWeb/inc/Style/assets/js/jquery.visible.js" charset="utf-8"></script>
+          <script type="text/javascript" src="ClubWeb/inc/Style/assets/js/jquery.visible.js" charset="utf-8"></script>
           <!-- Sticky Div -->
           <script type="text/javascript" src="ClubWeb/inc/Style/assets/js/jquery.sticky.js" charset="utf-8"></script>
           <!-- Slimbox2-->
