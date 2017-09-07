aaee69992f27a8778a47380336cb51880e7b92cc
Jumbo the code warlock <jamiepeterlaw@gmail.com>
2017-09-07 22:33:11 +0100

    remove frwrd slashes

--- index.php
+++ index.php
@@ -237,21 +237,21 @@ include("ClubWeb/inc/scripts/header.php");
           </div>
           <!-- End Footer -->
           <!-- JS -->
-          <script type="text/javascript" src="/ClubWeb/inc/scripts/Style/assets/js/jquery.min.js" type="text/javascript"></script>
-          <script type="text/javascript" src="/ClubWeb/inc/scripts/Style/assets/js/bootstrap.min.js" type="text/javascript"></script>
-          <script type="text/javascript" src="/ClubWeb/inc/scripts//Style/assets/js/scripts.js"></script>
+          <script type="text/javascript" src="ClubWeb/inc/scripts/Style/assets/js/jquery.min.js" type="text/javascript"></script>
+          <script type="text/javascript" src="ClubWeb/inc/scripts/Style/assets/js/bootstrap.min.js" type="text/javascript"></script>
+          <script type="text/javascript" src="ClubWeb/inc/scripts//Style/assets/js/scripts.js"></script>
           <!-- Isotope - Portfolio Sorting -->
-          <script type="text/javascript" src="/ClubWeb/inc/scripts/Style/assets/js/jquery.isotope.js" type="text/javascript"></script>
+          <script type="text/javascript" src="ClubWeb/inc/scripts/Style/assets/js/jquery.isotope.js" type="text/javascript"></script>
           <!-- Mobile Menu - Slicknav -->
-          <script type="text/javascript" src="/ClubWeb/inc/scripts/Style/assets/js/jquery.slicknav.js" type="text/javascript"></script>
+          <script type="text/javascript" src="ClubWeb/inc/scripts/Style/assets/js/jquery.slicknav.js" type="text/javascript"></script>
           <!-- Animate on Scroll-->
-          <script type="text/javascript" src="/ClubWeb/inc/scripts/Style/assets/js/jquery.visible.js" charset="utf-8"></script>
+          <script type="text/javascript" src="ClubWeb/inc/scripts/Style/assets/js/jquery.visible.js" charset="utf-8"></script>
           <!-- Sticky Div -->
-          <script type="text/javascript" src="/ClubWeb/inc/scripts/Style/assets/js/jquery.sticky.js" charset="utf-8"></script>
+          <script type="text/javascript" src="ClubWeb/inc/scripts/Style/assets/js/jquery.sticky.js" charset="utf-8"></script>
           <!-- Slimbox2-->
-          <script type="text/javascript" src="/ClubWeb/inc/scripts/Style/assets/js/slimbox2.js" charset="utf-8"></script>
+          <script type="text/javascript" src="ClubWeb/inc/scripts/Style/assets/js/slimbox2.js" charset="utf-8"></script>
           <!-- Modernizr -->
-          <script src="/ClubWeb/inc/scripts/Style/assets/js/modernizr.custom.js" type="text/javascript"></script>
+          <script src="ClubWeb/inc/scripts/Style/assets/js/modernizr.custom.js" type="text/javascript"></script>
           <!-- End JS -->
   </body>
 </html>
