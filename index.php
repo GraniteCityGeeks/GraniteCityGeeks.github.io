066383729627679273f82d554dfef1f5c6baf79b
Jumbo the code warlock <jamiepeterlaw@gmail.com>
2017-09-07 22:03:45 +0100

    file paths

--- index.php
+++ index.php
@@ -241,17 +241,17 @@ include("ClubWeb/inc/scripts/header.php");
           <script type="text/javascript" src="/ClubWeb/inc/Style/assets/js/bootstrap.min.js" type="text/javascript"></script>
           <script type="text/javascript" src="/ClubWeb/inc/Style/assets/js/scripts.js"></script>
           <!-- Isotope - Portfolio Sorting -->
-          <script type="text/javascript" src="ClubWeb/inc/Style/assets/js/jquery.isotope.js" type="text/javascript"></script>
+          <script type="text/javascript" src="/ClubWeb/inc/Style/assets/js/jquery.isotope.js" type="text/javascript"></script>
           <!-- Mobile Menu - Slicknav -->
-          <script type="text/javascript" src="ClubWeb/inc/Style/assets/js/jquery.slicknav.js" type="text/javascript"></script>
+          <script type="text/javascript" src="/ClubWeb/inc/Style/assets/js/jquery.slicknav.js" type="text/javascript"></script>
           <!-- Animate on Scroll-->
-          <script type="text/javascript" src="ClubWeb/inc/Style/assets/js/jquery.visible.js" charset="utf-8"></script>
+          <script type="text/javascript" src="/ClubWeb/inc/Style/assets/js/jquery.visible.js" charset="utf-8"></script>
           <!-- Sticky Div -->
-          <script type="text/javascript" src="ClubWeb/inc/Style/assets/js/jquery.sticky.js" charset="utf-8"></script>
+          <script type="text/javascript" src="/ClubWeb/inc/Style/assets/js/jquery.sticky.js" charset="utf-8"></script>
           <!-- Slimbox2-->
-          <script type="text/javascript" src="ClubWeb/inc/Style/assets/js/slimbox2.js" charset="utf-8"></script>
+          <script type="text/javascript" src="/ClubWeb/inc/Style/assets/js/slimbox2.js" charset="utf-8"></script>
           <!-- Modernizr -->
-          <script src="ClubWeb/inc/Style/assets/js/modernizr.custom.js" type="text/javascript"></script>
+          <script src="/ClubWeb/inc/Style/assets/js/modernizr.custom.js" type="text/javascript"></script>
           <!-- End JS -->
   </body>
 </html>
