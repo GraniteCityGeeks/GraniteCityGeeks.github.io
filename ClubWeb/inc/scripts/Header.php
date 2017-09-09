2580eec4a2a54568371665bd26c7a8353490f9a3
Jumbo the code warlock <jamiepeterlaw@gmail.com>
2017-09-09 13:19:32 +0100

    header admin

--- ClubWeb/inc/scripts/Header.php
+++ ClubWeb/inc/scripts/Header.php
@@ -97,6 +97,26 @@ session_start();
 
                                 </li>
 
+                                <?
+                                if (isset($_SESSION['accessLevelID']) == 2){
+                                    echo "<li><a href='../createclub'>Create Clubs</a></li>";
+                                    echo "<li><a href='../modifyclub'>modify your Club</a></li>";
+                                    echo "<li><a href='../MarkerAdmin'>Create Map Info</a></li>";
+                                } else if (isset($_SESSION['accessLevelID']) == 3){
+                                    echo "<li><a href='../MarkerAdmin'>Maps Editing</a></li>";
+                                } else if (isset($_SESSION['accessLevelID']) == 4){
+                                    echo "<li><a href='../createclub'>Create Club</a></li>";
+                                    echo "<li><a href='../create_club_article'>Creat Club Article</a></li>";
+                                } else if (isset($_SESSION['accessLevelID']) == 5){
+                                    echo "<li><a href='../createclub'>Create Clubs</a></li>";
+                                    echo "<li><a href='../create_club_article'>Creat Club Article</a></li>";
+                                    echo "<li><a href='../modifyclub'>modify your Club</a></li>";
+                                    echo "<li><a href='../MarkerAdmin'>Create Map Info</a></li>";
+                                    echo "<li><a href='../view'>View Users</a></li>";
+                                } else {
+                                }
+                                ?>
+
 
 
                             </ul>
