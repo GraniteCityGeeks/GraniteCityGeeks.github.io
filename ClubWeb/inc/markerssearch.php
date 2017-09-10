f32e0523c89809f2c2e98c1fd2eeec1fbf5ba165
Murray Lyne <murraylyne2@Gmail.com>
2017-09-10 20:31:05 +0100

    for future search function

---
+++ ClubWeb/inc/markerssearch.php
@@ -0,0 +1,34 @@
+<?php
+$query = $_POST['query']
+include("scripts/dbconnect.php");
+$dom = new DOMDocument("1.0");
+$node = $dom->createElement("markers");
+$parnode = $dom->appendChild($node);
+
+$query = "SELECT * from port_markers where name = '$query'";
+$result = $db->query($query);
+if (!$result) {
+    die('Nothing in result: ');
+}
+
+header("Content-type: text/xml");
+
+// Iterate through the rows, adding XML nodes for each
+
+while ($row = $result->fetch_array()){
+    // ADD TO XML DOCUMENT NODE
+    $node = $dom->createElement("markers");
+    $newnode = $parnode->appendChild($node);
+    $newnode->setAttribute("name",$row['name']);
+    $newnode->setAttribute("address", $row['address']);
+    $newnode->setAttribute("lat", $row['lat']);
+    $newnode->setAttribute("lng", $row['lng']);
+    $newnode->setAttribute("type", $row['type']);
+    $newnode->setAttribute("description", $row['description']);
+}
+$result->close();
+$db->close();
+
+echo $dom->saveXML();
+
+?>
