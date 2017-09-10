6d6184c540d4a0da488027bfda03df7984d484b9
Murray Lyne <murraylyne2@Gmail.com>
2017-09-10 19:06:05 +0100

    markers

--- ClubWeb/inc/MarkerAdmin.php
+++ ClubWeb/inc/MarkerAdmin.php
@@ -246,7 +246,7 @@ session_start();
 
          //marker
 
-            downloadUrl("markers", function(data) {
+            downloadUrl("http://gcg.azurewebsites.net/ClubWeb/markers", function(data) {
               var xml = data.responseXML;
               var markers = xml.documentElement.getElementsByTagName('marker');
               Array.prototype.forEach.call(markers, function(markerElem) {
