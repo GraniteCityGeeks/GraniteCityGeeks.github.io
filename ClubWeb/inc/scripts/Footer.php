e66f73f7c897f107d66065416175e2bdc153cce3
JordanYoungman <jwyoungman96@hotmail.co.uk>
2017-09-11 15:01:56 +0100

    footer

--- ClubWeb/inc/scripts/Footer.php
+++ ClubWeb/inc/scripts/Footer.php
@@ -1,7 +1,9 @@
 <footer class="footer-basic-centered">
+      <div class="footer">
 
     <p class="footer-company-motto">Discover Aberdeen</p>
 
     <p class="footer-company-name">GraniteCityGeeks &copy; 2016</p>
 
+      <div class="footer">
 </footer>
