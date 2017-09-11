2a18a60059b08fe8c026ea4e3c2cc1ff8e3bcd90
JordanYoungman <jwyoungman96@hotmail.co.uk>
2017-09-11 14:40:31 +0100

    clubs

--- ClubWeb/inc/viewclubs.php
+++ ClubWeb/inc/viewclubs.php
@@ -18,16 +18,17 @@ echo "<h1> Portlethen clubs </h1>";
 echo "<h4> Displaying ". $total . " active clubs </h4>";
 
 
-echo "<table>";
 
 while($row = $result->fetch_array()) {
     $pictureURL = $row['URL'];
     $clubID = $row['clubid'];
     $clubTitle = $row['clubTitle'];
 
-
-    echo "<td><img src='{$pictureURL}'height='300', width='300'></td>";
-    echo "<td><h1><a href='Clubs/{$clubID}' name='linkref' id='link'>{$clubTitle}</a></h1></td>";
+    echo "
+    <div class="clubs">
+    <img src='{$pictureURL}'height='300', width='300'>
+    <h1><a href='Clubs/{$clubID}' name='linkref' id='link'>{$clubTitle}</a></h1>
+    ";
 
 
 
@@ -41,7 +42,6 @@ while($row = $result->fetch_array()) {
 
 }
 
-echo "</table>";
 
 echo "</main>";
 
