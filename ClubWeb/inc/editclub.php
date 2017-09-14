d16e4939ffb2f05ea7b5b223e72b301520682377
JordanYoungman <jwyoungman96@hotmail.co.uk>
2017-09-14 14:22:42 +0100

    club

--- ClubWeb/inc/editclub.php
+++ ClubWeb/inc/editclub.php
@@ -1,13 +1,27 @@
 <?php
-if (isset($_SESSION['username'])) {
+include('scripts/dbconnect.php');
+session_start();
+if (isset ($_SESSION['username'])) {
 
-    if ($_SESSION['accessLevelID'] >= 1) {
-        //connect to the database.
-        include("/scripts/dbconnect.php");
-
-        $query = "SELECT * FROM port_club";
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        include("scripts/Header.php");
+        ?>
+        <main>
+            <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+        <?
+        //first check that the user is admin in club. Otherwise return the user back to viewclubs
+        //query to check that the user manages a club.
+        $id = $_SESSION['username'];
+        $query ="select * FROM port_club";
 
         $result = $db->query($query);
+
+        if (!$result) {
+            header('Location: Clubs');
+
+        } else  {
+            //if a club has been found, continue.
+            //get results from the query.
 
         while ($row = $result->fetch_array()) {
 
