216653db7c93d523539a630c82f3af9df1bee0c5
JordanYoungman <jwyoungman96@hotmail.co.uk>
2017-09-14 14:05:01 +0100

    hh
    hh

--- ClubWeb/inc/editclub.php
+++ ClubWeb/inc/editclub.php
@@ -1,7 +1,33 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: 1607084
- * Date: 09/12/2016
- * Time: 18:43
- */
+if (isset($_SESSION['username'])) {
+
+    if ($_SESSION['accessLevelID'] >= 3) {
+        //connect to the database.
+        include("/scripts/dbconnect.php");
+
+        $query = "SELECT * FROM port_club";
+
+        $result = $db->query($query);
+
+        while ($row = $result->fetch_array()) {
+
+            echo "<tr>";
+            echo "<td>" . $row["name"] . "</td>";
+            echo "<td>" . $row["description"] . "</td>";
+            echo "<td>" . $row["genre"] . "</td>";
+            echo "<td>" . $row["avatar"] . "</td>";
+            echo "<form action='Delete' method='POST'>";
+            echo "<td>" . "<button name='delete' type='submit' value='" . $row["id"] . "'>" . "delete" . "</button>" . "</td>";
+            echo "</form>";
+            echo "</tr>";
+
+        }
+
+
+        $db->close();
+
+
+        echo "</table>";
+    }
+}
+?>
--- ClubWeb/inc/scripts/Header.php
+++ ClubWeb/inc/scripts/Header.php
@@ -100,7 +100,7 @@ session_start();
                                 <?
                                 if (isset($_SESSION['accessLevelID']) == 2){
                                     echo "<li><a href='../ClubWeb/createclub'>Create Clubs</a></li>";
-                                    echo "<li><a href='../ClubWeb/Edit'>modify your Club</a></li>";
+                                    echo "<li><a href='../ClubWeb/editclub'>modify your Club</a></li>";
                                     echo "<li><a href='../ClubWeb/MarkerAdmin'>Create Map Info</a></li>";
                                 } else if (isset($_SESSION['accessLevelID']) == 3){
                                     echo "<li><a href='../ClubWeb/MarkerAdmin'>Maps Editing</a></li>";
