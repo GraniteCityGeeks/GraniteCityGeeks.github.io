13e34ea6b85818b05b2dff07f19895356eb9b964
Jumbo the code warlock <jamiepeterlaw@gmail.com>
2017-09-15 21:13:03 +0100

    fix

--- ClubWeb/inc/scripts/Header.php
+++ ClubWeb/inc/scripts/Header.php
@@ -110,17 +110,7 @@ session_start();
                                     echo "<li><a href='../ClubWeb/create_club_article'>Creat Club Article</a></li>";
                                 } else if (isset($_SESSION['accessLevelID']) == 5){
                                     echo "<li><a href='http://gcg.azurewebsites.net/ClubWeb/inc/Admin.php'>Admin</a></li>";
-                                    echo '<div class="btn-group">
-  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Action <span class="caret"></span>
-                                  </button>
-                                  <ul class="dropdown-menu">
-                                    <li><a href="#">Action</a></li>
-                                    <li><a href="#">Another action</a></li>
-                                    <li><a href="#">Something else here</a></li>
-                                    <li role="separator" class="divider"></li>
-                                    <li><a href="#">Separated link</a></li>'
-
+                                    
                                 } else {
                                 }
                                 ?>
