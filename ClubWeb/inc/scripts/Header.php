675848457efb84351ea299cf04d280e7919ada1f
Jumbo the code warlock <jamiepeterlaw@gmail.com>
2017-09-15 21:37:05 +0100

    header

--- ClubWeb/inc/scripts/Header.php
+++ ClubWeb/inc/scripts/Header.php
@@ -113,7 +113,7 @@ session_start();
                                       <h2>Dropdowns</h2>
                                       <p>The .dropdown class is used to indicate a dropdown menu.</p>
                                       <p>Use the .dropdown-menu class to actually build the dropdown menu.</p>
-                                      <p>To open the dropdown menu, use a button or a link with a class of .dropdown-toggle and data-toggle="dropdown".</p>
+                                      <p>To open the dropdown menu, use a button or a link with a class of .dropdown-toggle and data-toggle='dropdown'.</p>
                                       <div class='dropdown'>
                                         <button class='btn btn-primary dropdown-toggle' type='button' data-toggle='dropdown'>Dropdown Example
                                         <span class='caret'></span></button>
