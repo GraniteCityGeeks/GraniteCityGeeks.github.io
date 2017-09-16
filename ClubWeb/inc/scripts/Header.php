d19392ef8c97ec7315e2831110ca4f83f60cbca9
Jumbo the code warlock <jamiepeterlaw@gmail.com>
2017-09-16 23:48:26 +0100

    fix

--- ClubWeb/inc/scripts/Header.php
+++ ClubWeb/inc/scripts/Header.php
@@ -126,18 +126,14 @@ session_start();
                     </div>
                     <div class="col-md-4 no-padding">
                         <ul class="social-icons pull-right">
-                            <li class="social-rss">
-                                <a href="#" target="_blank" title="RSS"></a>
-                            </li>
+
                             <li class="social-twitter">
                                 <a href="#" target="_blank" title="Twitter"></a>
                             </li>
                             <li class="social-facebook">
                                 <a href="#" target="_blank" title="Facebook"></a>
                             </li>
-                            <li class="social-googleplus">
-                                <a href="#" target="_blank" title="Google+"></a>
-                            </li>
+                            
                         </ul>
                     </div>
                 </div>
